63fe5af9db73a9332612ff3a07b4d633b77ed458
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-10-24 14:54:38 +0200

    :hammer: chore: remove unused import

--- packages/client/src/components/leo/modals/CreateTicketModal.tsx
+++ packages/client/src/components/leo/modals/CreateTicketModal.tsx
@@ -8,7 +8,7 @@ import { Loader } from "components/Loader";
 import { Modal } from "components/modal/Modal";
 import { useModal } from "context/ModalContext";
 import { useValues } from "context/ValuesContext";
-import { Form, Formik, yupToFormErrors } from "formik";
+import { Form, Formik } from "formik";
 import { handleValidate } from "lib/handleValidate";
 import useFetch from "lib/useFetch";
 import { ModalIds } from "types/ModalIds";
