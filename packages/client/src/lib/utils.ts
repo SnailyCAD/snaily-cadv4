80947cf4b8cf664ad98bf2645bc644cdea6042d8
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-11-22 16:31:15 +0100

    :hammer: chore: log error

--- packages/client/src/lib/utils.ts
+++ packages/client/src/lib/utils.ts
@@ -56,7 +56,13 @@ export async function requestAll(req: any, config: Config) {
         req,
       })
         .then((v) => (typeof v.data === "undefined" ? defaultValue : v.data))
-        .catch(() => defaultValue);
+        .catch((error) => {
+          if (error) {
+            console.log("DEBUG: ", error);
+          }
+
+          return defaultValue;
+        });
     }),
   );
 }
