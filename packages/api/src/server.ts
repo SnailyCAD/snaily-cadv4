2111431999c49973103421092147cb75d2cfc4df
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-12-07 19:07:46 +0100

    :hammer: experimental stuff

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -11,6 +11,7 @@ const rootDir = __dirname;
 
 @Configuration({
   rootDir,
+  httpPort: process.env.USE_WEB_PROXY ? 80 : 8080,
   logger: {
     debug: true,
     level: process.env.NODE_ENV === "production" ? "off" : undefined,
