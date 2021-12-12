cf524179c9ffd17bebc011b5d99c108c1cd3ec99
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-12-12 08:48:06 +0100

    :hammer: add experimental .env flag

--- packages/api/src/utils/setCookie.ts
+++ packages/api/src/utils/setCookie.ts
@@ -9,12 +9,23 @@ interface SetCookieOptions {
 }
 
 export function setCookie(options: SetCookieOptions) {
+  let serializeOptions = {};
+
+  // this will break the CAD if you're not using https!
+  if (process.env.EXPERIMENTAL_SECURE_CONTEXT) {
+    serializeOptions = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+
   options.res.setHeader(
     "Set-Cookie",
     serialize(options.name, options.value, {
       httpOnly: true,
       expires: new Date(Date.now() + options.expires),
       path: "/",
+      ...serializeOptions,
     }),
   );
 }
