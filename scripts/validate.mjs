6646a22063f882dff25e5fa2e4f4a5d31146bff8
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-12-15 15:18:05 +0100

    :hammer: add validation script to warn when installing invalid Node.js version

---
+++ scripts/validate.mjs
@@ -0,0 +1,18 @@
+import chalk from "chalk";
+
+const [major, minor] = process.version.split(".").map((v) => {
+  return parseInt(v.replace("v", ""));
+});
+
+const is16 = major >= 16;
+const isDot6 = minor >= 6;
+const isSupportedVersion = is16 && isDot6;
+const versionText = chalk.bold(chalk.underline("16.6 or higher"));
+
+if (!isSupportedVersion) {
+  throw warn(`Unsupported Node.js version detected. SnailyCADv4 requires version ${versionText}.`);
+}
+
+function warn(message) {
+  console.warn(`${chalk.yellow("warn")} -`, message);
+}
