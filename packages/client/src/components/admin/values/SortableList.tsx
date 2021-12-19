eeb5cd60890a95fd175c6c54c4f7947b0de39a30
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-12-19 14:08:46 +0100

    :hammer: minor spam fix

--- packages/client/src/components/admin/values/SortableList.tsx
+++ packages/client/src/components/admin/values/SortableList.tsx
@@ -15,21 +15,39 @@ interface ListProps {
 export const SortableList = ({ values, search, setList, handleEdit, handleDelete }: ListProps) => {
   const common = useTranslations("Common");
 
+  function checkMoved(list: TValue[]) {
+    let wasMoved = false;
+
+    for (let i = 0; i < values.length; i++) {
+      if (values[i]?.id !== list[i]?.id) {
+        wasMoved = true;
+        break;
+      }
+    }
+
+    /**
+     * only update db if the list was actually moved.
+     */
+    if (wasMoved) {
+      setList(list);
+    }
+  }
+
   return (
-    <ReactSortable animation={200} className="mt-5" tag="ul" list={values} setList={setList}>
+    <ReactSortable animation={200} className="mt-5" tag="ul" list={values} setList={checkMoved}>
       {sortValues(values)
         .filter((v) => handleFilter(v, search))
         .map((value, idx) => (
           <li
-            className="my-1 bg-gray-200 dark:bg-gray-2 p-2 px-4 rounded-md flex items-center justify-between"
+            className="flex items-center justify-between p-2 px-4 my-1 bg-gray-200 rounded-md dark:bg-gray-2"
             key={value.id}
           >
             <div className="flex items-center">
               <span className="cursor-move">
-                <ArrowsExpand className="text-gray-500 mr-2" width={15} />
+                <ArrowsExpand className="mr-2 text-gray-500" width={15} />
               </span>
 
-              <span className="select-none text-gray-500">{++idx}.</span>
+              <span className="text-gray-500 select-none">{++idx}.</span>
               <span className="ml-2">
                 {typeof value.value !== "string" && value.value.type === "DIVISION" ? (
                   <span>{(value as any).department.value?.value} / </span>
