c3c8542e6e57a4488174073de052c5ca8eb57c19
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-12-23 19:33:38 +0100

    :hammer: enable prisma errors

--- packages/api/src/lib/prisma.ts
+++ packages/api/src/lib/prisma.ts
@@ -2,6 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 const isDevelopment = process.env.NODE_ENV === "development";
 
-const log = (isDevelopment ? ["info", "warn", "error"] : ["info"]) as any[];
+const log = (isDevelopment ? ["info", "warn", "error"] : ["info", "error"]) as any[];
 
 export const prisma = new PrismaClient({ errorFormat: "pretty", log });
