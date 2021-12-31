9d9fb7710a1d835856ce4b4dfe35daab2af82ace
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-12-31 09:00:18 +0100

    :hammer: minor changes

--- packages/client/src/pages/officer/supervisor/citizen-logs.tsx
+++ packages/client/src/pages/officer/supervisor/citizen-logs.tsx
@@ -43,15 +43,6 @@ export default function CitizenLogs({ logs: data }: Props) {
 
       <header className="flex items-center justify-between">
         <h1 className="mb-3 text-3xl font-semibold">{t("citizenLogs")}</h1>
-
-        <div>
-          {currentLog ? (
-            <Button className="flex items-center gap-2" onClick={() => setCurrentLog(null)}>
-              <ArrowLeft />
-              {"View all"}
-            </Button>
-          ) : null}
-        </div>
       </header>
 
       {logs.length <= 0 ? (
@@ -63,57 +54,72 @@ export default function CitizenLogs({ logs: data }: Props) {
           </FormField>
 
           {currentLog ? (
-            <Table
-              filter={search}
-              defaultSort={{
-                columnId: "createdAt",
-                descending: true,
-              }}
-              data={data
-                .filter((v) => v.citizenId === currentLog.citizenId)
-                .map((item) => {
-                  const type = item.records !== null ? TYPE_LABELS[item.records.type] : "Warrant";
-                  const createdAt = item.warrant?.createdAt ?? item.records?.createdAt;
-                  const formatted = format(new Date(createdAt!), "yyyy-MM-dd HH:mm:ss");
-                  const officer = item.warrant?.officer ?? item.records?.officer;
-                  const officerName = officer && makeUnitName(officer);
-                  const callsign = officer && generateCallsign(officer);
+            <>
+              <div className="flex items-center justify-between mt-5">
+                <h3 className="text-xl font-semibold">
+                  {currentLog ? `${currentLog.citizen.name} ${currentLog.citizen.surname}` : null}
+                </h3>
 
-                  const extra = item.records
-                    ? {
-                        status: "—",
-                        postal: item.records.postal || common("none"),
-                        notes: item.records.notes || common("none"),
-                        violations:
-                          item.records.violations.map((v) => v.penalCode.title).join(", ") ||
-                          common("none"),
-                      }
-                    : {
-                        postal: "—",
-                        status: item.warrant?.status,
-                        violations: "—",
-                        notes: "—",
-                      };
+                {currentLog ? (
+                  <Button className="flex items-center gap-2" onClick={() => setCurrentLog(null)}>
+                    <ArrowLeft />
+                    {"View all"}
+                  </Button>
+                ) : null}
+              </div>
 
-                  return {
-                    type,
-                    citizen: `${item.citizen.name} ${item.citizen.surname}`,
-                    officer: `${callsign} ${officerName}`,
-                    ...extra,
-                    createdAt: formatted,
-                  };
-                })}
-              columns={[
-                { Header: common("type"), accessor: "type" },
-                { Header: t("citizen"), accessor: "citizen" },
-                { Header: t("officer"), accessor: "officer" },
-                { Header: t("postal"), accessor: "postal" },
-                { Header: t("status"), accessor: "status" },
-                { Header: t("notes"), accessor: "notes" },
-                { Header: t("violations"), accessor: "violations" },
-                { Header: common("createdAt"), accessor: "createdAt" },
-              ]}
-            />
+              <Table
+                filter={search}
+                defaultSort={{
+                  columnId: "createdAt",
+                  descending: true,
+                }}
+                data={data
+                  .filter((v) => v.citizenId === currentLog.citizenId)
+                  .map((item) => {
+                    const type = item.records !== null ? TYPE_LABELS[item.records.type] : "Warrant";
+                    const createdAt = item.warrant?.createdAt ?? item.records?.createdAt;
+                    const formatted = format(new Date(createdAt!), "yyyy-MM-dd HH:mm:ss");
+                    const officer = item.warrant?.officer ?? item.records?.officer;
+                    const officerName = officer && makeUnitName(officer);
+                    const callsign = officer && generateCallsign(officer);
+
+                    const extra = item.records
+                      ? {
+                          status: "—",
+                          postal: item.records.postal || common("none"),
+                          notes: item.records.notes || common("none"),
+                          violations:
+                            item.records.violations.map((v) => v.penalCode.title).join(", ") ||
+                            common("none"),
+                        }
+                      : {
+                          postal: "—",
+                          status: item.warrant?.status,
+                          violations: "—",
+                          notes: "—",
+                        };
+
+                    return {
+                      type,
+                      citizen: `${item.citizen.name} ${item.citizen.surname}`,
+                      officer: `${callsign} ${officerName}`,
+                      ...extra,
+                      createdAt: formatted,
+                    };
+                  })}
+                columns={[
+                  { Header: common("type"), accessor: "type" },
+                  { Header: t("citizen"), accessor: "citizen" },
+                  { Header: t("officer"), accessor: "officer" },
+                  { Header: t("postal"), accessor: "postal" },
+                  { Header: t("status"), accessor: "status" },
+                  { Header: t("notes"), accessor: "notes" },
+                  { Header: t("violations"), accessor: "violations" },
+                  { Header: common("createdAt"), accessor: "createdAt" },
+                ]}
+              />
+            </>
           ) : (
             <Table
               filter={search}
