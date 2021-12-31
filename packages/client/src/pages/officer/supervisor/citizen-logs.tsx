faddb06e97a7df03bc1e527db9482e612cf8336c
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-12-31 08:44:47 +0100

    :tada: add citizen view

--- packages/client/src/pages/officer/supervisor/citizen-logs.tsx
+++ packages/client/src/pages/officer/supervisor/citizen-logs.tsx
@@ -7,19 +7,27 @@ import { getSessionUser } from "lib/auth";
 import { getTranslations } from "lib/getTranslation";
 import { makeUnitName, requestAll } from "lib/utils";
 import { GetServerSideProps } from "next";
-import { Citizen, RecordLog } from "types/prisma";
+import { Citizen, RecordLog, RecordType } from "types/prisma";
 import { Table } from "components/table/Table";
 import { FormField } from "components/form/FormField";
 import { Input } from "components/form/Input";
 import format from "date-fns/format";
 import { useGenerateCallsign } from "hooks/useGenerateCallsign";
+import { ArrowLeft } from "react-bootstrap-icons";
 
 type Log = RecordLog & { citizen: Citizen };
 interface Props {
   logs: Log[];
 }
 
-export default function CitizenLogs({ logs }: Props) {
+const TYPE_LABELS = {
+  [RecordType.TICKET]: "Ticket",
+  [RecordType.ARREST_REPORT]: "Arrest Report",
+  [RecordType.WRITTEN_WARNING]: "Written Warning",
+};
+
+export default function CitizenLogs({ logs: data }: Props) {
+  const logs = React.useMemo(() => uniqueList(data), [data]);
   const [search, setSearch] = React.useState("");
   const [currentLog, setCurrentLog] = React.useState<Log | null>(null);
 
@@ -32,9 +40,22 @@ export default function CitizenLogs({ logs }: Props) {
       <Head>
         <title>{t("citizenLogs")} - SnailyCAD</title>
       </Head>
-      <h1 className="mb-3 text-3xl font-semibold">{t("citizenLogs")}</h1>
+
+      <header className="flex items-center justify-between">
+        <h1 className="mb-3 text-3xl font-semibold">{t("citizenLogs")}</h1>
+
+        <div>
+          {currentLog ? (
+            <Button className="flex items-center gap-2" onClick={() => setCurrentLog(null)}>
+              <ArrowLeft />
+              {"View all"}
+            </Button>
+          ) : null}
+        </div>
+      </header>
+
       {logs.length <= 0 ? (
-        <p className="mt-5">{t("noImpoundedVehicles")}</p>
+        <p className="mt-5">{t("noLogs")}</p>
       ) : (
         <>
           <FormField label={common("search")} className="my-2">
@@ -44,31 +65,53 @@ export default function CitizenLogs({ logs }: Props) {
           {currentLog ? (
             <Table
               filter={search}
-              data={logs
+              defaultSort={{
+                columnId: "createdAt",
+                descending: true,
+              }}
+              data={data
                 .filter((v) => v.citizenId === currentLog.citizenId)
                 .map((item) => {
+                  const type = item.records !== null ? TYPE_LABELS[item.records.type] : "Warrant";
                   const createdAt = item.warrant?.createdAt ?? item.records?.createdAt;
                   const formatted = format(new Date(createdAt!), "yyyy-MM-dd HH:mm:ss");
                   const officer = item.warrant?.officer ?? item.records?.officer;
                   const officerName = officer && makeUnitName(officer);
                   const callsign = officer && generateCallsign(officer);
 
+                  const extra = item.records
+                    ? {
+                        status: "—",
+                        postal: item.records.postal || common("none"),
+                        notes: item.records.notes || common("none"),
+                        violations:
+                          item.records.violations.map((v) => v.penalCode.title).join(", ") ||
+                          common("none"),
+                      }
+                    : {
+                        postal: "—",
+                        status: item.warrant?.status,
+                        violations: "—",
+                        notes: "—",
+                      };
+
                   return {
+                    type,
                     citizen: `${item.citizen.name} ${item.citizen.surname}`,
                     officer: `${callsign} ${officerName}`,
+                    ...extra,
                     createdAt: formatted,
-                    actions: (
-                      <>
-                        <Button onClick={() => setCurrentLog(item)}>{common("view")}</Button>
-                      </>
-                    ),
                   };
                 })}
               columns={[
+                { Header: common("type"), accessor: "type" },
                 { Header: t("citizen"), accessor: "citizen" },
                 { Header: t("officer"), accessor: "officer" },
+                { Header: t("postal"), accessor: "postal" },
+                { Header: t("status"), accessor: "status" },
+                { Header: t("notes"), accessor: "notes" },
+                { Header: t("violations"), accessor: "violations" },
                 { Header: common("createdAt"), accessor: "createdAt" },
-                { Header: common("actions"), accessor: "actions" },
               ]}
             />
           ) : (
@@ -119,3 +162,19 @@ export const getServerSideProps: GetServerSideProps = async ({ req, locale }) =>
     },
   };
 };
+
+function uniqueList(logs: Log[]) {
+  const arr: Log[] = [];
+
+  for (let i = 0; i < logs.length; i++) {
+    const citizenId = logs[i]?.citizenId;
+
+    if (arr.some((v) => v.citizenId === citizenId)) {
+      continue;
+    }
+
+    arr.push(logs[i]!);
+  }
+
+  return arr;
+}
