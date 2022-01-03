23c1327fc575f8c39c10d319da29a8c6744cdd6a
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-01-03 12:48:01 +0100

    :hammer: 1 migration

--- packages/api/prisma/migrations/20220102124915_/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "ExpungementRequestStatus" AS ENUM ('ACCEPTED', 'DENIED', 'PENDING');
-
--- AlterTable
-ALTER TABLE "ExpungementRequest" ADD COLUMN     "status" "ExpungementRequestStatus" NOT NULL DEFAULT E'PENDING';
--- packages/api/prisma/migrations/20220103114738_/migration.sql
+++ packages/api/prisma/migrations/20220103114738_/migration.sql
@@ -1,5 +1,12 @@
+-- CreateEnum
+CREATE TYPE "ExpungementRequestStatus" AS ENUM ('ACCEPTED', 'DENIED', 'PENDING');
+
 -- AlterTable
 ALTER TABLE "Record" ADD COLUMN     "expungementRequestId" TEXT;
+
+-- AlterTable
+ALTER TABLE "RecordLog" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
 -- AlterTable
 ALTER TABLE "Warrant" ADD COLUMN     "expungementRequestId" TEXT;
@@ -8,6 +15,10 @@ ALTER TABLE "Warrant" ADD COLUMN     "expungementRequestId" TEXT;
 CREATE TABLE "ExpungementRequest" (
     "id" TEXT NOT NULL,
     "citizenId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "status" "ExpungementRequestStatus" NOT NULL DEFAULT E'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "ExpungementRequest_pkey" PRIMARY KEY ("id")
 );
@@ -20,3 +31,6 @@ ALTER TABLE "Warrant" ADD CONSTRAINT "Warrant_expungementRequestId_fkey" FOREIGN
 
 -- AddForeignKey
 ALTER TABLE "ExpungementRequest" ADD CONSTRAINT "ExpungementRequest_citizenId_fkey" FOREIGN KEY ("citizenId") REFERENCES "Citizen"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ExpungementRequest" ADD CONSTRAINT "ExpungementRequest_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
