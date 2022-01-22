a686cabd6ed367a2cff90c9cc7153ff52fee1474
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-01-22 14:23:04 +0100

    :hammer: chore: fetch CAD on 404 page (closes #314)

--- packages/client/src/pages/403.tsx
+++ packages/client/src/pages/403.tsx
@@ -1,7 +1,8 @@
 import { Layout } from "components/Layout";
 import { Title } from "components/shared/Title";
+import { handleRequest } from "lib/fetch";
 import { getTranslations } from "lib/getTranslation";
-import { GetStaticProps } from "next";
+import { GetServerSideProps } from "next";
 
 export default function FourOhFour() {
   return (
@@ -13,9 +14,14 @@ export default function FourOhFour() {
   );
 }
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => {
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  const { data } = await handleRequest("/admin/manage/cad-settings").catch(() => ({
+    data: null,
+  }));
+
   return {
     props: {
+      cad: data ?? {},
       messages: {
         ...(await getTranslations(["common"], locale)),
       },
--- packages/client/src/pages/404.tsx
+++ packages/client/src/pages/404.tsx
@@ -1,9 +1,31 @@
+import * as React from "react";
 import { Layout } from "components/Layout";
 import { GetStaticProps } from "next";
 import { getTranslations } from "lib/getTranslation";
 import { Title } from "components/shared/Title";
+import { handleRequest } from "lib/fetch";
+import { useAuth } from "context/AuthContext";
 
 export default function FourOhFour() {
+  const { setCad } = useAuth();
+
+  /**
+   * the 404 page can only be static due to Next.js' restrictions :/
+   */
+  const fetchCAD = React.useCallback(async () => {
+    const { data } = await handleRequest("/admin/manage/cad-settings").catch(() => ({
+      data: null,
+    }));
+
+    if (data) {
+      setCad(data);
+    }
+  }, [setCad]);
+
+  React.useEffect(() => {
+    fetchCAD();
+  }, [fetchCAD]);
+
   return (
     <Layout className="dark:text-white">
       <Title>Page not found.</Title>
