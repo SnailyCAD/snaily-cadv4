784d33d996c7c6e547abecf5655f109874cf2d09
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-01-22 08:07:49 +0100

    :hammer: 1 migration

--- packages/api/prisma/migrations/20220122065425_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `tow` on the `AutoSetPropertiesUser` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "AutoSetPropertiesUser" DROP COLUMN "tow";
