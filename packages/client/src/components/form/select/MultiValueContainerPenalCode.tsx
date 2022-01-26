453caa5ea4dc1c3b62ea82ab7eecec3c9004975b
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-01-26 10:22:05 +0100

    :hammer: chore: remove duplicated HoverCard

--- packages/client/src/components/form/select/MultiValueContainerPenalCode.tsx
+++ packages/client/src/components/form/select/MultiValueContainerPenalCode.tsx
@@ -1,38 +1,24 @@
 import { components, MultiValueGenericProps } from "react-select";
-import * as HoverCard from "@radix-ui/react-hover-card";
+import { HoverCard } from "components/shared/HoverCard";
 import { PenalCode } from "types/prisma";
 import { dataToSlate, Editor } from "components/modal/DescriptionModal/Editor";
 
 export function MultiValueContainerPenalCode(props: MultiValueGenericProps<any>) {
-  const value = props.data.value as PenalCode;
+  const penalCode = props.data.value as PenalCode;
 
   return (
-    <HoverCardDemo penalCode={value}>
-      <div>
-        <components.MultiValueContainer {...props} />
-      </div>
-    </HoverCardDemo>
-  );
-}
-
-interface HoverCardProps {
-  children: React.ReactNode;
-  penalCode: PenalCode;
-}
-
-const HoverCardDemo = ({ children, penalCode }: HoverCardProps) => (
-  <HoverCard.Root closeDelay={10} openDelay={0}>
-    <HoverCard.Trigger asChild>{children}</HoverCard.Trigger>
-    <HoverCard.Content
-      className="bg-gray-200 dark:bg-dark-bright shadow-lg w-full max-w-2xl p-3 rounded-md dark:text-white pointer-events-none"
-      sideOffset={7}
+    <HoverCard
+      trigger={
+        <div>
+          <components.MultiValueContainer {...props} />
+        </div>
+      }
     >
-      <h3 className="text-lg font-semibold">{penalCode.title}</h3>
+      <h3 className="text-lg font-semibold px-2">{penalCode.title}</h3>
 
       <div className="dark:text-gray-200 mt-2 text-base">
         <Editor isReadonly value={dataToSlate(penalCode)} />
       </div>
-      <HoverCard.Arrow className="fill-current text-white dark:text-dark-bright" />
-    </HoverCard.Content>
-  </HoverCard.Root>
-);
+    </HoverCard>
+  );
+}
