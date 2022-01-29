72d99687ec64612a5a3d592ce389eff29f654036
Casper <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-01-29 10:00:16 +0100

    🎉 feat: editor upgrades (#351)

---
+++ packages/client/src/lib/editor/withShortcuts.ts
@@ -0,0 +1,103 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-condition */
+import type { ReactEditor } from "slate-react";
+import { Editor, Transforms, Range, Point, Element as SlateElement, type BaseEditor } from "slate";
+
+const SHORTCUTS = {
+  "*": "list-item",
+  "-": "list-item",
+  "+": "list-item",
+  ">": "block-quote",
+  "#": "heading-one",
+  "##": "heading-two",
+};
+
+export const withShortcuts = (editor: BaseEditor & ReactEditor) => {
+  const { deleteBackward, insertText } = editor;
+
+  editor.insertText = (text) => {
+    const { selection } = editor;
+
+    if (text === " " && selection && Range.isCollapsed(selection)) {
+      const { anchor } = selection;
+      const block = Editor.above(editor, {
+        match: (n) => Editor.isBlock(editor, n),
+      });
+      const path = block ? block[1] : [];
+      const start = Editor.start(editor, path);
+      const range = { anchor, focus: start };
+      const beforeText = Editor.string(editor, range);
+      const type = SHORTCUTS[beforeText as keyof typeof SHORTCUTS];
+
+      if (type) {
+        Transforms.select(editor, range);
+        Transforms.delete(editor);
+        const newProperties = {
+          type,
+        };
+
+        Transforms.setNodes<SlateElement>(editor, newProperties as any, {
+          match: (n) => Editor.isBlock(editor, n),
+        });
+
+        if (type === "list-item") {
+          const list = {
+            type: "bulleted-list",
+            children: [],
+          };
+
+          Transforms.wrapNodes(editor, list as any, {
+            match: (n) =>
+              !Editor.isEditor(n) && SlateElement.isElement(n) && (n as any).type === "list-item",
+          });
+        }
+
+        return;
+      }
+    }
+
+    insertText(text);
+  };
+
+  editor.deleteBackward = (...args) => {
+    const { selection } = editor;
+
+    if (selection && Range.isCollapsed(selection)) {
+      const match = Editor.above(editor, {
+        match: (n) => Editor.isBlock(editor, n),
+      });
+
+      if (match) {
+        const [block, path] = match;
+        const start = Editor.start(editor, path);
+
+        if (
+          !Editor.isEditor(block) &&
+          SlateElement.isElement(block) &&
+          block.type !== "paragraph" &&
+          Point.equals(selection.anchor, start)
+        ) {
+          const newProperties: Partial<SlateElement> = {
+            type: "paragraph",
+          };
+          Transforms.setNodes(editor, newProperties);
+
+          if (block.type === "list-item") {
+            Transforms.unwrapNodes(editor, {
+              match: (n) =>
+                !Editor.isEditor(n) &&
+                SlateElement.isElement(n) &&
+                (n as any).type === "bulleted-list",
+              split: true,
+            });
+          }
+
+          return;
+        }
+      }
+
+      deleteBackward(...args);
+    }
+  };
+
+  return editor;
+};
