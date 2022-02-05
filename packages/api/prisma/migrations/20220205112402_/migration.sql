a4f1f6049bf4c4b9a1cd432b7f036548674f8340
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-02-05 13:45:08 +0100

    :hammer: 1 migration

--- packages/api/prisma/migrations/20220205112402_/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "TableActionsAlignment" AS ENUM ('NONE', 'LEFT', 'RIGHT');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "tableActionsAlignment" "TableActionsAlignment" NOT NULL DEFAULT E'LEFT';
