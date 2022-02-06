ef81c32736d79591b9161bd0e3820158825d4450
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-02-06 20:22:01 +0100

    :tada: minor progress

--- packages/api/src/controllers/admin/manage/cad-settings/discord/DiscordSettingsController.ts
+++ packages/api/src/controllers/admin/manage/cad-settings/discord/DiscordSettingsController.ts
@@ -1,12 +1,14 @@
 import process from "node:process";
-import { Context, Controller, UseBeforeEach } from "@tsed/common";
-import { Get } from "@tsed/schema";
+import { BodyParams, Context, Controller, UseBeforeEach } from "@tsed/common";
+import { Get, Post } from "@tsed/schema";
 import { request } from "undici";
 import { Routes } from "discord-api-types/v9";
 import { IsAuth } from "middlewares/IsAuth";
 import { prisma } from "lib/prisma";
 import type { cad } from "@prisma/client";
 import { BadRequest } from "@tsed/exceptions";
+import { DISCORD_SETTINGS_SCHEMA } from "@snailycad/schemas";
+import { validateSchema } from "lib/validateSchema";
 
 const DISCORD_API_VERSION = "v9";
 const guildId = process.env.DISCORD_SERVER_ID;
@@ -64,5 +66,50 @@ export class DiscordSettingsController {
     }
 
     return data;
+  }
+
+  @Post("/")
+  async setRoleTypes(@Context("cad") cad: cad, @BodyParams() body: unknown) {
+    const data = validateSchema(DISCORD_SETTINGS_SCHEMA, body);
+
+    if (!guildId || !botToken) {
+      throw new BadRequest(
+        // todo: add link
+        "Must set `DISCORD_BOT_TOKEN` and `DISCORD_SERVER_ID` in .env file. See docs: ",
+      );
+    }
+
+    const roles = await request(`${discordApiUrl}${Routes.guildRoles(guildId)}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bot ${botToken}`,
+      },
+    });
+    const rolesBody = await roles.body.json();
+
+    Object.values(data).map((roleId) => {
+      if (roleId && !this.doesRoleExist(rolesBody, roleId)) {
+        throw new BadRequest("invalidRoleId");
+      }
+    });
+
+    const createUpdateData = {
+      guildId,
+      dispatchRoleId: data.dispatchRoleId ?? null,
+      leoRoleId: data.leoRoleId ?? null,
+      emsFdRoleId: data.emsFdRoleId ?? null,
+    };
+
+    const discordRoles = await prisma.discordRoles.upsert({
+      where: { id: String(cad.discordRolesId) },
+      update: createUpdateData,
+      create: createUpdateData,
+    });
+
+    return discordRoles;
+  }
+
+  protected doesRoleExist(roles: { id: string }[], roleId: string) {
+    return roles.some((v) => v.id === roleId);
   }
 }
