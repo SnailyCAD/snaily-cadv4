7362fb5863c76aaff7f7704d0574d5d3227d9248
subtosharki <camywam@yahoo,com>
2022-02-10 16:13:22 -0500

    adding custom port

--- scripts/copy-env.mjs
+++ scripts/copy-env.mjs
@@ -1,50 +1,69 @@
 import { one } from "copy";
 import { join } from "node:path";
+import "dotenv/config";
+import * as fs from "fs";
 
-const [, , ...args] = process.argv;
-const copyToClient = hasArg("--client");
-const copyToApi = hasArg("--api");
+fs.readFile("./packages/client/package.json", "utf8", (err, json) => {
 
-let ENV_FILE_PATH = join(process.cwd(), ".env");
-
-if (
-  ENV_FILE_PATH.endsWith("/packages/client/.env") ||
-  ENV_FILE_PATH.endsWith("/packages/api/.env")
-) {
-  ENV_FILE_PATH = ENV_FILE_PATH.replace(/packages\/(client|api)\//, "");
-}
-
-/**
- * @param {string} distDir
- */
-async function copyEnv(distDir) {
-  try {
-    one(ENV_FILE_PATH, distDir, (error) => {
-      if (error) {
-        console.log({ error });
-        return;
+  let port = process.env.PORT_CLIENT;
+  if (!port) {
+    return;
+  } else {
+    json = JSON.parse(json);
+    json.scripts.start = `next start -p ${port}`;
+    json = JSON.stringify(json, null, 2);
+    fs.writeFile("./packages/client/package.json", json, (err) => {
+      if (err) {
+        console.log(err);
       }
-
-      const type =
-        (distDir.endsWith("client") && "client") || (distDir.endsWith("api") && "api") || "Unknown";
-
-      console.log(`✅ copied .env — ${type}`);
     });
-  } catch (e) {
-    console.log({ e });
   }
-}
+});
 
-if (copyToClient) {
-  const CLIENT_PACKAGE_PATH = join(process.cwd(), "packages", "client");
-  copyEnv(CLIENT_PACKAGE_PATH);
-}
+// const [, , ...args] = process.argv;
+// const copyToClient = hasArg("--client");
+// const copyToApi = hasArg("--api");
 
-if (copyToApi) {
-  const API_PACKAGE_PATH = join(process.cwd(), "packages", "api");
-  copyEnv(API_PACKAGE_PATH);
-}
+// let ENV_FILE_PATH = join(process.cwd(), ".env");
 
-function hasArg(arg) {
-  return args.includes(arg);
-}
+// if (
+//   ENV_FILE_PATH.endsWith("/packages/client/.env") ||
+//   ENV_FILE_PATH.endsWith("/packages/api/.env")
+// ) {
+//   ENV_FILE_PATH = ENV_FILE_PATH.replace(/packages\/(client|api)\//, "");
+// }
+
+// /**
+//  * @param {string} distDir
+//  */
+// async function copyEnv(distDir) {
+//   try {
+//     one(ENV_FILE_PATH, distDir, (error) => {
+//       if (error) {
+//         console.log({ error });
+//         return;
+//       }
+
+//       const type =
+//         (distDir.endsWith("client") && "client") || (distDir.endsWith("api") && "api") || "Unknown";
+
+//       console.log(`✅ copied .env — ${type}`);
+//     });
+//   } catch (e) {
+//     console.log({ e });
+//   }
+// }
+
+// if (copyToClient) {
+//   const CLIENT_PACKAGE_PATH = join(process.cwd(), "packages", "client");
+//   copyEnv(CLIENT_PACKAGE_PATH);
+// }
+
+// if (copyToApi) {
+//   const API_PACKAGE_PATH = join(process.cwd(), "packages", "api");
+//   copyEnv(API_PACKAGE_PATH);
+// }
+
+// function hasArg(arg) {
+//   return args.includes(arg);
+// }
