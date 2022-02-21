b61d94f4cb69940cc1c1bbb4647c54e05ecd472f
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-02-21 19:26:32 +0100

    :hammer: 1 migration

--- packages/api/prisma/migrations/20220221165741_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Officer" ADD COLUMN     "activeIncidentId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Officer" ADD CONSTRAINT "Officer_activeIncidentId_fkey" FOREIGN KEY ("activeIncidentId") REFERENCES "LeoIncident"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- packages/api/prisma/migrations/20220221182451_/migration.sql
+++ packages/api/prisma/migrations/20220221182451_/migration.sql
@@ -8,5 +8,11 @@ ALTER TABLE "LeoIncident" DROP CONSTRAINT "LeoIncident_creatorId_fkey";
 ALTER TABLE "LeoIncident" ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT false,
 ALTER COLUMN "creatorId" DROP NOT NULL;
 
+-- AlterTable
+ALTER TABLE "Officer" ADD COLUMN     "activeIncidentId" TEXT;
+
+-- AddForeignKey
+ALTER TABLE "Officer" ADD CONSTRAINT "Officer_activeIncidentId_fkey" FOREIGN KEY ("activeIncidentId") REFERENCES "LeoIncident"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
 -- AddForeignKey
 ALTER TABLE "LeoIncident" ADD CONSTRAINT "LeoIncident_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "Officer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
