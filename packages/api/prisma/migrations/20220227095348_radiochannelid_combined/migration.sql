5799552975190e0bd39af420bd4c77d76a3553bd
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-02-27 11:16:20 +0100

    :hammer: chore: 1 migration

--- packages/api/prisma/migrations/20220227095348_radiochannelid_combined/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `radioChannel` on the `CombinedLeoUnit` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "CombinedLeoUnit" DROP COLUMN "radioChannel",
-ADD COLUMN     "radioChannelId" TEXT;
