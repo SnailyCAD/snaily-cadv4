38e226b3d42f3ac61c5bbc4124396ef3229b85d4
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-03-08 19:48:33 +0100

    :hammer: test(utils): add tests for generateCallsign

---
+++ packages/utils/tests/callsign.test.ts
@@ -0,0 +1,50 @@
+/* eslint-disable quotes */
+import { expect, test } from "vitest";
+import { generateCallsign } from "../src/callsign";
+
+// simple test data
+const OFFICER = {
+  callsign: "E",
+  callsign2: "92",
+  citizen: { name: "john", surname: "doe" },
+  badgeNumber: 6034,
+  department: { value: { value: "LSPD" }, callsign: "A" },
+  divisions: [{ value: { value: "Patrol" }, callsign: "P" }],
+  citizenId: "xxxxx",
+} as any;
+
+const EMS_FD_DEPUTY = {
+  callsign: "E",
+  callsign2: "92",
+  citizen: { name: "john", surname: "doe" },
+  badgeNumber: 6034,
+  department: { value: { value: "LSPD" }, callsign: "A" },
+  division: { value: { value: "Patrol" }, callsign: "P" },
+  citizenId: "xxxxx",
+} as any;
+
+const COMBINED_UNIT = {
+  callsign: OFFICER.callsign,
+  callsign2: OFFICER.callsign2,
+  department: OFFICER.department,
+  officers: [OFFICER],
+} as any;
+
+const DEFAULT_TEMPLATE = "{department}{callsign1} - {callsign2}{division}";
+const PAIRED_UNIT_TEMPLATE = "1A-{callsign1}";
+
+test(`should generate LEO callsign template: ${DEFAULT_TEMPLATE}`, () => {
+  expect(generateCallsign(OFFICER, DEFAULT_TEMPLATE)).toMatchInlineSnapshot('"AE - 92P"');
+});
+
+test(`should generate EMS/FD callsign template: ${DEFAULT_TEMPLATE}`, () => {
+  expect(generateCallsign(EMS_FD_DEPUTY, DEFAULT_TEMPLATE)).toMatchInlineSnapshot('"AE - 92P"');
+});
+
+test(`should generate paired unit callsign template: ${PAIRED_UNIT_TEMPLATE}`, () => {
+  expect(generateCallsign(COMBINED_UNIT, PAIRED_UNIT_TEMPLATE)).toMatchInlineSnapshot('"1A-E"');
+});
+
+test(`should return "" if template === null`, () => {
+  expect(generateCallsign(COMBINED_UNIT, null)).toMatchInlineSnapshot('""');
+});
