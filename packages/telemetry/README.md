5ad325019228b9a04e6b79da88d66d5850641cab
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-03-27 10:46:11 +0200

    :hammer: chore: add default report data

--- packages/telemetry/README.md
+++ packages/telemetry/README.md
@@ -1,3 +1,3 @@
-# @snailycad/telementry
+# @snailycad/telemetry
 
 TODO
--- packages/telemetry/src/index.ts
+++ packages/telemetry/src/index.ts
@@ -1,1 +1,49 @@
-export {};
+import process from "node:process";
+import os from "node:os";
+import { execSync } from "node:child_process";
+import { get, set } from "./cache";
+
+const TELEMETRY_ENABLED = process.env.TELEMETRY_ENABLED === "true";
+
+interface ErrorReport {
+  name: string;
+  message: string;
+  stack?: string;
+}
+
+export async function report(errorReport: ErrorReport) {
+  if (!TELEMETRY_ENABLED) return;
+
+  const [yarn, node, npm] = await Promise.all([
+    getBinaryVersions("yarn"),
+    getBinaryVersions("node"),
+    getBinaryVersions("npm"),
+  ]);
+
+  const data = {
+    yarn,
+    npm,
+    node,
+    platform: os.platform(),
+    os: os.platform(),
+    ...errorReport,
+  };
+
+  data;
+}
+
+async function getBinaryVersions(command: "yarn" | "node" | "npm") {
+  const cache = get(command);
+
+  if (cache) {
+    return cache;
+  }
+
+  try {
+    const out = execSync(`${command} -v`, { encoding: "utf-8" }).toString().trim();
+    set(command, out);
+    return out;
+  } catch {
+    return null;
+  }
+}
