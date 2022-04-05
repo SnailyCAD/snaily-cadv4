31eda46a71634fe9154816058c95e349a2393b55
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-04-05 07:59:22 +0200

    :hammer: chore: add migration

--- packages/client/src/components/leo/ActiveCalls.tsx
+++ packages/client/src/components/leo/ActiveCalls.tsx
@@ -182,7 +182,7 @@ function ActiveCallsInner() {
                   rowProps: {
                     className: isUnitAssigned ? "bg-gray-200 dark:bg-[#333639]" : undefined,
                   },
-                  caseNumber: call.caseNumber,
+                  caseNumber: `#${call.caseNumber}`,
                   name: `${call.name} ${call.viaDispatch ? `(${leo("dispatch")})` : ""}`,
                   location: call.location,
                   description:
@@ -249,7 +249,7 @@ function ActiveCallsInner() {
                 };
               })}
             columns={[
-              { Header: t("caseNumber"), accessor: "caseNumber" },
+              { Header: "#", accessor: "caseNumber" },
               { Header: t("caller"), accessor: "name" },
               { Header: t("location"), accessor: "location" },
               { Header: common("description"), accessor: "description" },
