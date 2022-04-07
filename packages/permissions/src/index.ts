4fb68a38966b0a9ced418f0de587d49a8781a5fe
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-04-07 13:08:18 +0200

    :hammer: chore: generate simple code comments

--- packages/permissions/src/index.ts
+++ packages/permissions/src/index.ts
@@ -2,6 +2,13 @@ import * as defaultPermissions from "./defaults";
 import { allPermissions, type Permissions } from "./permissions";
 
 export * from "./permissions";
+
+/**
+ * checks whether a user has certain permissions.
+ * @param userPermissions - An array of permissions that the user has.
+ * @param toCheck - The permissions you want to check for.
+ * @returns whether a user has certain permissions.
+ */
 export function hasPermission(
   userPermissions: readonly (string | Permissions)[],
   toCheck: readonly Permissions[],
@@ -23,6 +30,12 @@ export function hasPermission(
 }
 
 export type PermissionNames = keyof typeof Permissions;
+
+/**
+ * given a list of permissions, return a dictionary of permissions
+ * @param userPermissions - An array of permissions that the user has.
+ * @returns A dictionary of permissions.
+ */
 export function getPermissions(
   userPermissions: readonly (string | Permissions)[],
 ): Record<PermissionNames, boolean> {
