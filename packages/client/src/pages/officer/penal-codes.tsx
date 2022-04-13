b258f46c41b3c81f5e1206176d04cab3130f98c0
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-04-13 14:55:31 +0200

    :hammer: chore: add search

--- packages/client/src/pages/officer/penal-codes.tsx
+++ packages/client/src/pages/officer/penal-codes.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { Layout } from "components/Layout";
 import { Title } from "components/shared/Title";
 import { Permissions } from "@snailycad/permissions";
@@ -8,10 +9,26 @@ import { requestAll } from "lib/utils";
 import { getSessionUser } from "lib/auth";
 import { getTranslations } from "lib/getTranslation";
 import { useValues } from "context/ValuesContext";
+import { FormField } from "components/form/FormField";
+import { Input } from "components/form/inputs/Input";
 
 export default function PenalCodesPage() {
   const t = useTranslations("Leo");
+  const common = useTranslations("Common");
   const { penalCode } = useValues();
+
+  const [search, setSearch] = React.useState("");
+  const [filtered, setFiltered] = React.useState(penalCode.values);
+
+  React.useEffect(() => {
+    if (!search) {
+      setFiltered(penalCode.values);
+    }
+
+    setFiltered(
+      penalCode.values.filter((v) => v.title.toLowerCase().includes(search.toLowerCase())),
+    );
+  }, [search, penalCode.values]);
 
   return (
     <Layout
@@ -27,15 +44,21 @@ export default function PenalCodesPage() {
       {penalCode.values.length <= 0 ? (
         <p className="mt-5">{t("noPenalCodes")}</p>
       ) : (
-        <ul className="flex flex-col mt-5">
-          {penalCode.values.map((penalCode) => (
-            <li className="card p-4" key={penalCode.id}>
-              <h3 className="text-2xl font-semibold">{penalCode.title}</h3>
+        <>
+          <FormField label={common("search")} className="my-2">
+            <Input onChange={(e) => setSearch(e.target.value)} value={search} />
+          </FormField>
 
-              <Editor isReadonly value={penalCode.descriptionData!} />
-            </li>
-          ))}
-        </ul>
+          <ul className="flex flex-col mt-5">
+            {filtered.map((penalCode) => (
+              <li className="card p-4" key={penalCode.id}>
+                <h3 className="text-2xl font-semibold">{penalCode.title}</h3>
+
+                <Editor isReadonly value={penalCode.descriptionData!} />
+              </li>
+            ))}
+          </ul>
+        </>
       )}
     </Layout>
   );
