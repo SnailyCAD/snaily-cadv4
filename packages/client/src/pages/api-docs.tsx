ca518a63230c962334ca076d3bf46c5a1224d520
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-04-14 11:09:35 +0200

    :hammer: chore: redirect to API-docs if entered via client

---
+++ packages/client/src/pages/api-docs.tsx
@@ -0,0 +1,25 @@
+import { Loader } from "components/Loader";
+import { findUrl } from "lib/fetch";
+import type { GetServerSideProps } from "next";
+
+export default function ApiDocs() {
+  return (
+    <div className="fixed inset-0 grid bg-transparent place-items-center">
+      <span aria-label="redirecting...">
+        <Loader className="w-14 h-14 border-[3px]" />
+      </span>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const apiURL = findUrl();
+  const destination = apiURL.replace("/v1", "/api-docs");
+
+  return {
+    redirect: {
+      destination,
+      permanent: true,
+    },
+  };
+};
