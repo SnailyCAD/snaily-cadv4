475953048c25b48ad408ce2c2f99b0bcda413041
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-04-19 16:35:18 +0200

    :tada: feat: add to admin layout

--- packages/client/src/components/admin/AdminLayout.tsx
+++ packages/client/src/components/admin/AdminLayout.tsx
@@ -5,6 +5,7 @@ import { classNames } from "lib/classNames";
 import { AdminSidebar } from "./Sidebar";
 import type { LayoutProps } from "components/Layout";
 import { useHasPermissionForLayout } from "hooks/auth/useHasPermissionForLayout";
+import { useSocketError } from "hooks/global/useSocketError";
 
 interface Props {
   children: React.ReactNode;
@@ -14,6 +15,7 @@ interface Props {
 
 export function AdminLayout({ children, className, permissions }: Props) {
   const { Component, audio, roleplayStopped } = useRoleplayStopped();
+  const { SocketErrorComponent, showError } = useSocketError();
   const { forbidden, Loader } = useHasPermissionForLayout(permissions);
 
   if (forbidden) {
@@ -30,6 +32,8 @@ export function AdminLayout({ children, className, permissions }: Props) {
 
           <div className="ml-6 px-4 py-5 admin-dashboard-responsive">
             {roleplayStopped ? <Component audio={audio} /> : null}
+            {showError ? <SocketErrorComponent /> : null}
+
             {children}
           </div>
         </div>
