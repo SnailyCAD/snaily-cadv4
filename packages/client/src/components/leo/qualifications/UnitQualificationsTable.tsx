9627bfcfaa21b0e4755f3682acfd980125c6a41c
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-04-27 17:38:42 +0200

    :hammer: chore: separate qualifications and awards

--- packages/client/src/components/leo/qualifications/UnitQualificationsTable.tsx
+++ packages/client/src/components/leo/qualifications/UnitQualificationsTable.tsx
@@ -1,4 +1,4 @@
-import type { EmsFdDeputy, Officer, UnitQualification } from "@snailycad/types";
+import { EmsFdDeputy, Officer, QualificationValueType, UnitQualification } from "@snailycad/types";
 import { QualificationsHoverCard } from "components/admin/manage/units/QualificationHoverCard";
 import { FullDate } from "components/shared/FullDate";
 import { Table } from "components/shared/Table";
@@ -10,42 +10,70 @@ interface Props {
 }
 
 export function UnitQualificationsTable({ unit }: Props) {
+  const t = useTranslations("Leo");
+
+  const awards = unit.qualifications.filter(
+    (v) => v.qualification.qualificationType === QualificationValueType.AWARD,
+  );
+
+  const qualifications = unit.qualifications.filter(
+    (v) => v.qualification.qualificationType === QualificationValueType.QUALIFICATION,
+  );
+
+  return (
+    <div className="mt-3">
+      <div id="qualifications">
+        <h2 className="text-xl font-semibold">{t("unitQualifications")}</h2>
+
+        {!qualifications.length ? (
+          <p className="my-2 text-gray-400">{t("noQualifications")}</p>
+        ) : (
+          <QualificationAwardsTable data={qualifications} />
+        )}
+      </div>
+
+      <div id="awards">
+        <h2 className="text-xl font-semibold">{t("unitAwards")}</h2>
+
+        {!awards.length ? (
+          <p className="my-2 text-gray-400">{t("noAwards")}</p>
+        ) : (
+          <QualificationAwardsTable data={awards} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+function QualificationAwardsTable({ data }: { data: UnitQualification[] }) {
   const common = useTranslations("Common");
   const t = useTranslations("Leo");
 
   return (
-    <div className="mt-3">
-      <h1 className="text-xl font-semibold">{t("unitQualifications")}</h1>
-
-      {!unit.qualifications.length ? (
-        <p className="my-2 text-gray-400">{t("noQualifications")}</p>
-      ) : (
-        <Table
-          data={unit.qualifications.map((qa) => {
-            return {
-              image: <QualificationsHoverCard qualification={qa} />,
-              name: (
-                <p className="flex flex-col">
-                  <span className={classNames(qa.suspendedAt && "text-gray-400 line-through")}>
-                    {qa.qualification.value.value}
-                  </span>
-                  {qa.suspendedAt ? (
-                    <span>
-                      {t("suspendedOn")} <FullDate>{qa.suspendedAt}</FullDate>
-                    </span>
-                  ) : null}
-                </p>
-              ),
-              assignedAt: <FullDate>{qa.createdAt}</FullDate>,
-            };
-          })}
-          columns={[
-            { Header: common("image"), accessor: "image" },
-            { Header: common("name"), accessor: "name" },
-            { Header: t("assignedAt"), accessor: "assignedAt" },
-          ]}
-        />
-      )}
-    </div>
+    <Table
+      data={data.map((qa) => {
+        return {
+          image: <QualificationsHoverCard qualification={qa} />,
+          name: (
+            <p className="flex flex-col">
+              <span className={classNames(qa.suspendedAt && "text-gray-400 line-through")}>
+                {qa.qualification.value.value}
+              </span>
+              {qa.suspendedAt ? (
+                <span>
+                  {t("suspendedOn")} <FullDate>{qa.suspendedAt}</FullDate>
+                </span>
+              ) : null}
+            </p>
+          ),
+          assignedAt: <FullDate>{qa.createdAt}</FullDate>,
+        };
+      })}
+      columns={[
+        { Header: common("image"), accessor: "image" },
+        { Header: common("name"), accessor: "name" },
+        { Header: t("assignedAt"), accessor: "assignedAt" },
+      ]}
+    />
   );
 }
