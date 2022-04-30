990ae25aeb7c238c1491b27e51271746eab67045
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-04-30 13:47:24 +0200

    :bug: fix: hide user API tokens if disabled (closes feature/sna-17076)

---
+++ packages/client/src/components/admin/manage/users/ApiTokenArea.tsx
@@ -0,0 +1,44 @@
+import type { User } from "@snailycad/types";
+import { FullDate } from "components/shared/FullDate";
+import { Infofield } from "components/shared/Infofield";
+import { Table } from "components/shared/Table";
+
+interface Props {
+  user: User;
+}
+
+export function ApiTokenArea({ user }: Props) {
+  return (
+    <div className="p-4 mt-10 bg-gray-200 rounded-md dark:bg-gray-2">
+      <h1 className="text-2xl font-semibold">API Token</h1>
+
+      {user.apiToken ? (
+        <div className="mt-2">
+          <Infofield label="Uses">{String(user.apiToken.uses ?? 0)}</Infofield>
+          <Infofield label="Created At">
+            <FullDate>{user.apiToken.createdAt}</FullDate>
+          </Infofield>
+
+          {!user.apiToken.logs?.length ? null : (
+            <Table
+              data={user.apiToken.logs.map((log) => ({
+                route: (
+                  <span className="font-mono">
+                    <span className="font-semibold">{log.method}</span> {log.route}
+                  </span>
+                ),
+                createdAt: <FullDate>{log.createdAt}</FullDate>,
+              }))}
+              columns={[
+                { Header: "Route", accessor: "route" },
+                { Header: "Created At", accessor: "createdAt" },
+              ]}
+            />
+          )}
+        </div>
+      ) : (
+        <p className="text-gray-400 mt-2">User has no API Token set.</p>
+      )}
+    </div>
+  );
+}
--- packages/client/src/pages/admin/manage/users/[id].tsx
+++ packages/client/src/pages/admin/manage/users/[id].tsx
@@ -28,9 +28,8 @@ import { usePermission, Permissions } from "hooks/usePermission";
 import dynamic from "next/dynamic";
 import { SettingsFormField } from "components/form/SettingsFormField";
 import { AlertModal } from "components/modal/AlertModal";
-import { FullDate } from "components/shared/FullDate";
-import { Infofield } from "components/shared/Infofield";
-import { Table } from "components/shared/Table";
+import { ApiTokenArea } from "components/admin/manage/users/ApiTokenArea";
+import { useFeatureEnabled } from "hooks/useFeatureEnabled";
 
 const DangerZone = dynamic(
   async () => (await import("components/admin/manage/users/DangerZone")).DangerZone,
@@ -53,6 +52,7 @@ export default function ManageCitizens(props: Props) {
   const { user: session } = useAuth();
   const { openModal, closeModal } = useModal();
   const { hasPermissions } = usePermission();
+  const { USER_API_TOKENS } = useFeatureEnabled();
 
   React.useEffect(() => {
     if (!user) {
@@ -248,37 +248,7 @@ export default function ManageCitizens(props: Props) {
 
         <ManagePermissionsModal user={user} />
 
-        <div className="p-4 mt-10 bg-gray-200 rounded-md dark:bg-gray-2">
-          <h1 className="text-2xl font-semibold">API Token</h1>
-
-          {user.apiToken ? (
-            <div className="mt-2">
-              <Infofield label="Uses">{String(user.apiToken.uses ?? 0)}</Infofield>
-              <Infofield label="Created At">
-                <FullDate>{user.apiToken.createdAt}</FullDate>
-              </Infofield>
-
-              {!user.apiToken.logs?.length ? null : (
-                <Table
-                  data={user.apiToken.logs.map((log) => ({
-                    route: (
-                      <span className="font-mono">
-                        <span className="font-semibold">{log.method}</span> {log.route}
-                      </span>
-                    ),
-                    createdAt: <FullDate>{log.createdAt}</FullDate>,
-                  }))}
-                  columns={[
-                    { Header: "Route", accessor: "route" },
-                    { Header: "Created At", accessor: "createdAt" },
-                  ]}
-                />
-              )}
-            </div>
-          ) : (
-            <p className="text-gray-400 mt-2">User has no API Token set.</p>
-          )}
-        </div>
+        {USER_API_TOKENS ? <ApiTokenArea user={user} /> : null}
 
         {user.rank !== Rank.OWNER ? (
           <>
