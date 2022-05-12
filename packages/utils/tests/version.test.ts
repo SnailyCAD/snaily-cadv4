b683e38e413eb68cc1aaff996cea0705cb267bd2
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-05-12 17:30:51 +0200

    fix tests

--- packages/utils/tests/version.test.ts
+++ packages/utils/tests/version.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from "vitest";
 import { getCADVersion } from "../src/version";
 
 test("Should return the CAD version", async () => {
-  expect(await getCADVersion()).toBeTypeOf("string");
+  expect(await getCADVersion()).toBeTypeOf("object");
 });
