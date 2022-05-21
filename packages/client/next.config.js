585960f1b9a4d015e92623c70d521ddb4509de4e
Enzo Bonolis <104458119+SunlightKim@users.noreply.github.com>
2022-05-21 15:28:13 +0200

    French Translation (#796)

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
-    locales: ["en", "en_gb", "ru", "cn", "tc"],
+    locales: ["en", "en_gb", "ru", "cn", "tc", "fr_FRA"],
     defaultLocale: "en",
   },
   cleanDistDir: true,
