a4d3d16183a9580eddee40b3b3930a8092db2143
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-05-27 08:12:31 +0200

    :hammer: chore: add localhost error alert

--- packages/client/src/pages/auth/register.tsx
+++ packages/client/src/pages/auth/register.tsx
@@ -18,6 +18,7 @@ import { handleRequest } from "lib/fetch";
 import { Title } from "components/shared/Title";
 import { AuthScreenImages } from "components/auth/AuthScreenImages";
 import { useFeatureEnabled } from "hooks/useFeatureEnabled";
+import { LocalhostDetector } from "components/auth/LocalhostDetector";
 
 const INITIAL_VALUES = {
   username: "",
@@ -91,6 +92,7 @@ export default function Register({ cad }: Props) {
 
       <main className="flex flex-col items-center justify-center pt-20">
         <AuthScreenImages />
+        <LocalhostDetector />
 
         <Formik validate={validate} onSubmit={onSubmit} initialValues={INITIAL_VALUES}>
           {({ handleChange, errors, isValid }) => (
