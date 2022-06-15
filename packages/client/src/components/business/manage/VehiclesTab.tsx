4538e74801d79e513a82363ac1c48e2f72a0faef
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-06-15 13:23:30 +0200

    :tada: feat: able to transfer vehicles

--- packages/client/src/components/business/manage/VehiclesTab.tsx
+++ packages/client/src/components/business/manage/VehiclesTab.tsx
@@ -7,7 +7,7 @@ import { useModal } from "state/modalState";
 import { ModalIds } from "types/ModalIds";
 import type { RegisteredVehicle } from "@snailycad/types";
 import useFetch from "lib/useFetch";
-import { RegisterVehicleModal } from "components/citizen/vehicles/RegisterVehicleModal";
+import { RegisterVehicleModal } from "components/citizen/vehicles/modals/RegisterVehicleModal";
 import { AlertModal } from "components/modal/AlertModal";
 import { FullDate } from "components/shared/FullDate";
 import { Table } from "components/shared/Table";
