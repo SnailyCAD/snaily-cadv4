5ee958429fef63fb8a940d31a715a4fcbd3b99a8
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-06-16 11:17:50 +0200

    :hammer: chore: improve hook workflows

--- packages/client/src/hooks/useAsyncValues.ts
+++ packages/client/src/hooks/useAsyncValues.ts
@@ -1,4 +1,5 @@
 import * as React from "react";
+import create from "zustand";
 import type { ValueType } from "@snailycad/types";
 import useFetch from "lib/useFetch";
 import { useValues } from "context/ValuesContext";
@@ -8,22 +9,50 @@ interface Options {
   id: string;
 }
 
-const ASYNC_VALUES_HOOK_USED_TIMES = 2;
-let fetchCount = 0;
+/** this will check against the store's `valuesToFetch`, this is wacky, but works for now. */
+const AMOUNT_OF_LOADABLE_VALUES = 7;
+let hasFetched = false;
+
+interface Store {
+  valuesToFetch: ValueType[];
+  addValuesToFetch(...values: ValueType[]): void;
+}
+
+const useStore = create<Store>((set, get) => ({
+  valuesToFetch: [],
+  addValuesToFetch: (...values) => {
+    const state = get();
+    return set({
+      valuesToFetch: [...state.valuesToFetch, ...values],
+    });
+  },
+}));
 
 export function useAsyncValues(options: Options) {
   const { state, execute } = useFetch();
   const { setValues } = useValues();
+  const store = useStore();
+
+  React.useEffect(() => {
+    store.addValuesToFetch(...options.valueTypes);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  React.useEffect(() => {
+    if (store.valuesToFetch.length === AMOUNT_OF_LOADABLE_VALUES && !hasFetched) {
+      fetchValues();
+      hasFetched = true;
+    }
+  }, [store.valuesToFetch.length]); // eslint-disable-line react-hooks/exhaustive-deps
 
   function transformValueTypesToString() {
-    return options.valueTypes
-      .slice(1, options.valueTypes.length)
+    return [...new Set(store.valuesToFetch)]
+      .slice(1, store.valuesToFetch.length)
       .map((type) => type.toLowerCase())
       .join(",");
   }
 
   const fetchValues = React.useCallback(async () => {
-    const [first, ...rest] = options.valueTypes;
+    const [first, ...rest] = store.valuesToFetch;
     const hasMore = rest.length >= 1;
 
     const { json } = await execute(
@@ -38,15 +67,7 @@ export function useAsyncValues(options: Options) {
     if (Array.isArray(json)) {
       setValues((prev) => [...prev, ...json]);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  React.useEffect(() => {
-    if (fetchCount <= ASYNC_VALUES_HOOK_USED_TIMES - 1) {
-      fetchValues();
-      fetchCount += 1;
-    }
-  }, [fetchValues]);
+  }, [store.valuesToFetch]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return { state };
 }
