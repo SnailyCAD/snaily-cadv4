206a31499176cd5fb64cd122541ab61a5387edd1
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-06-20 17:24:43 +0200

    :hammer: chore: add createAuditLog function

--- packages/audit-logger/src/core/server.ts
+++ packages/audit-logger/src/core/server.ts
@@ -1,9 +1,24 @@
+import superjson from "superjson";
 import type { PrismaClient } from "@prisma/client";
 import type { AuditLogActions } from "./actions";
 
 interface Options {
   prisma: PrismaClient;
+
+  executorId: string;
   action: AuditLogActions;
 }
 
-export async function createAuditLogEntry(options: Options) {}
+export async function createAuditLogEntry(options: Options) {
+  const auditLog = await options.prisma.auditLog.create({
+    data: {
+      action: superjson.stringify(options.action),
+      executorId: options.executorId,
+    },
+  });
+
+  return {
+    ...auditLog,
+    action: options.action,
+  };
+}
