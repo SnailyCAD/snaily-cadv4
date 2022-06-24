5d101c1eae670710495b91c569bd4a3733db66f6
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-06-24 08:14:01 +0200

    :bug: fix: return warning when clearing all divisions (closes #930)

--- packages/api/src/controllers/citizen/CitizenController.ts
+++ packages/api/src/controllers/citizen/CitizenController.ts
@@ -1,7 +1,7 @@
 import process from "node:process";
 import { UseBeforeEach, Context, MultipartFile, PlatformMulterFile } from "@tsed/common";
 import { Controller } from "@tsed/di";
-import { Delete, Get, Post, Put } from "@tsed/schema";
+import { Delete, Description, Get, Post, Put } from "@tsed/schema";
 import { QueryParams, BodyParams, PathParams } from "@tsed/platform-params";
 import { prisma } from "lib/prisma";
 import { IsAuth } from "middlewares/IsAuth";
@@ -71,6 +71,7 @@ export const citizenInclude = {
 @UseBeforeEach(IsAuth)
 export class CitizenController {
   @Get("/")
+  @Description("Get all the citizens of the authenticated user")
   async getCitizens(
     @Context("cad") cad: { features?: CadFeature[] },
     @Context("user") user: User,
