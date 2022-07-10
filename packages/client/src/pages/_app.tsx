9f76cba2e0e64156b452bd89fb0b79bbc4163dfb
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-07-10 09:07:32 +0200

    :tada: feat: add optional Google Captcha (closes #965)

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -7,7 +7,6 @@ import { AuthProvider } from "context/AuthContext";
 import { ValuesProvider } from "context/ValuesContext";
 import { CitizenProvider } from "context/CitizenContext";
 import "styles/globals.scss";
-import "styles/fonts.scss";
 import { SocketProvider } from "@casper124578/use-socket.io";
 import { findAPIUrl } from "lib/fetch";
 
@@ -15,6 +14,7 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 Sentry.init({
   dsn: "https://6e31d0dc886d482091e293edb73eb10e@o518232.ingest.sentry.io/6553264",
@@ -38,7 +38,13 @@ export default function App({ Component, router, pageProps }: AppProps) {
             <ValuesProvider initialData={pageProps}>
               <CitizenProvider initialData={pageProps}>
                 <DndProvider backend={HTML5Backend}>
-                  <Component {...pageProps} />
+                  <GoogleReCaptchaProvider
+                    reCaptchaKey={process.env.NEXT_PUBLIC_GOOGLE_CAPTCHA_SITE_KEY}
+                    scriptProps={{ async: true, defer: true, appendTo: "body" }}
+                    useRecaptchaNet
+                  >
+                    <Component {...pageProps} />
+                  </GoogleReCaptchaProvider>
                 </DndProvider>
                 <Toaster position="top-right" />
               </CitizenProvider>
