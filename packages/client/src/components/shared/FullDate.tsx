0376cc327112fe11d44ea0e0014ec7eb7071451b
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-07-29 09:00:12 +0200

    :bug: fix: render dates on client

--- packages/client/src/components/shared/FullDate.tsx
+++ packages/client/src/components/shared/FullDate.tsx
@@ -1,3 +1,4 @@
+import { useMounted } from "@casper124578/useful";
 import type { HoverCardProps } from "@radix-ui/react-hover-card";
 import format from "date-fns/format";
 import { formatDate } from "lib/utils";
@@ -10,19 +11,19 @@ interface Props extends Omit<HoverCardProps, "children"> {
 }
 
 export function FullDate({ children, onlyDate, isDateOfBirth }: Props) {
+  const isMounted = useMounted();
   const hmsString = onlyDate ? "" : "HH:mm:ss";
   let date = new Date(children).getTime();
 
   if (isDateOfBirth) {
     date = date + 5 * 60 * 60 * 1000;
   }
+
   const formatted = format(new Date(date), `EEEE, MMMM dd, yyyy ${hmsString}`);
+  const trigger = formatDate(children, { onlyDate: onlyDate ?? false });
 
   return (
-    <HoverCard
-      openDelay={100}
-      trigger={<span>{formatDate(children, { onlyDate: onlyDate ?? false })}</span>}
-    >
+    <HoverCard openDelay={100} trigger={<span>{isMounted ? trigger : null}</span>}>
       <span className="font-semibold">{formatted}</span>
     </HoverCard>
   );
