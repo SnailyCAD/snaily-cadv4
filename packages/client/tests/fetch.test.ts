526016c948a6b669d7eed0c8aced805e5cd42149
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-07-29 11:00:10 +0200

    remove useless test

--- packages/client/tests/fetch.test.ts
+++ packages/client/tests/fetch.test.ts
@@ -9,10 +9,3 @@ test.fails("Should return error instance", async () => {
 test("Should find the correct development URL", () => {
   expect(findAPIUrl()).toMatchInlineSnapshot('"http://localhost:8080/v1"');
 });
-
-test("Should find the correct development URL via Docker", () => {
-  process.env.NEXT_PUBLIC_PROD_ORIGIN = "http://api:8080/v1";
-  // @ts-expect-error testing purposes
-  global.window = {};
-  expect(findAPIUrl()).toMatchInlineSnapshot('"http://localhost:8080/v1"');
-});
