2c4ef107135b349d9db1fbcc7825299f8481cfea
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-08-21 12:39:59 +0200

    🎉 feat: allow webp images uploaded

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -8,7 +8,13 @@ export const API_TOKEN_HEADER = "snaily-cad-api-token" as const;
 export const USER_API_TOKEN_HEADER = "snaily-cad-user-api-token" as const;
 
 export type AllowedFileExtension = typeof allowedFileExtensions[number];
-export const allowedFileExtensions = ["image/png", "image/gif", "image/jpeg", "image/jpg"] as const;
+export const allowedFileExtensions = [
+  "image/png",
+  "image/gif",
+  "image/jpeg",
+  "image/jpg",
+  "image/webp",
+] as const;
 export const IMGUR_REGEX = /https:\/\/i.imgur.com\/[A-Za-z0-9]\w+.(jpeg|png|gif|jpg)/g;
 
 export * from "./socket-events";
