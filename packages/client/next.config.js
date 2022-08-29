1633711944d57d448b0e1f1ff9d54869917eb7c8
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-08-29 07:27:16 +0200

    :hammer: chore: minor fixes

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-condition */
 const { withSentryConfig } = require("@sentry/nextjs");
 
 /**
@@ -24,11 +25,12 @@ const nextConfig = {
 };
 
 /** @type {typeof nextConfig} */
-module.exports = withSentryConfig(nextConfig, {
-  org: "snailycad",
-  setCommits: true,
-  project: "snailycad-client",
-  authToken: "bce2b8a2e79f4336a3b115f171e675ab639d2d1f809a40919ca4493257a9c0e0",
-  release: "default",
-  silent: true,
-});
+module.exports =
+  withSentryConfig?.(nextConfig, {
+    org: "snailycad",
+    setCommits: true,
+    project: "snailycad-client",
+    authToken: "bce2b8a2e79f4336a3b115f171e675ab639d2d1f809a40919ca4493257a9c0e0",
+    release: "default",
+    silent: true,
+  }) ?? nextConfig;
