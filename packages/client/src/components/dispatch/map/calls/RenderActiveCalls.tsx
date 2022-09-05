6ab91dc72d162402ccf5afe680ed98aa87e1856f
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-09-05 18:52:11 +0200

    :tada: feat: minor map improvements

--- packages/client/src/components/dispatch/map/calls/RenderActiveCalls.tsx
+++ packages/client/src/components/dispatch/map/calls/RenderActiveCalls.tsx
@@ -53,7 +53,30 @@ export function RenderActiveCalls() {
   }
 
   async function handleMarkerChange(call: Full911Call, type: "remove" | "set") {
-    const coords = convertToMap(0, 0, map);
+    let coords = convertToMap(0, 0, map);
+
+    const positions = calls.map((c) => c.position);
+
+    let isMiddleOccupied = false;
+
+    function parseFloatToInt(x: number | null | undefined) {
+      return parseInt(String(x), 10);
+    }
+
+    for (const position of positions) {
+      console.log({ position, coords });
+
+      if (
+        parseFloatToInt(position?.lat) === parseFloatToInt(coords.lat) &&
+        parseFloatToInt(position?.lng) === parseFloatToInt(coords.lng)
+      ) {
+        isMiddleOccupied = true;
+        coords = convertToMap(150, 0, map);
+        break;
+      }
+    }
+
+    console.log({ isMiddleOccupied });
 
     const callData =
       type === "set"
