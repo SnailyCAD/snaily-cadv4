b032849f59aa6da441781da1a19d3d81a0d2d73c
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-09-22 16:42:31 +0200

    :hammer: chore: handle exceptions for getting translations

--- apps/client/src/lib/getTranslation.ts
+++ apps/client/src/lib/getTranslation.ts
@@ -5,22 +5,35 @@ import { getNextI18nConfig } from "./i18n/getNextI18nConfig";
 const cwd = process.cwd();
 
 export async function getTranslations(types: string[], locale = "en") {
-  const typesWithCommon = [...new Set(["common", "auth", ...types])];
-  const paths = typesWithCommon.map((type) => path.join(cwd, `locales/${locale}/${type}.json`));
-  const i18n = await getNextI18nConfig();
+  try {
+    const typesWithCommon = [...new Set(["common", "auth", ...types])];
+    const paths = typesWithCommon.map((type) => path.join(cwd, `locales/${locale}/${type}.json`));
+    const i18n = await getNextI18nConfig();
 
-  if (!i18n.locales.includes(locale)) {
-    locale = i18n.defaultLocale;
+    if (!i18n.locales.includes(locale)) {
+      locale = i18n.defaultLocale;
+    }
+
+    let data = {};
+
+    await Promise.all(
+      paths.map(async (path) => {
+        const json = await loadTranslationPath(path);
+        data = { ...data, ...json };
+      }),
+    );
+
+    return data;
+  } catch (error) {
+    return {};
   }
+}
 
-  let data = {};
-
-  await Promise.all(
-    paths.map(async (path) => {
-      const json = JSON.parse(await readFile(path, "utf8"));
-      data = { ...data, ...json };
-    }),
-  );
-
-  return data;
+async function loadTranslationPath(path: string) {
+  try {
+    const json = JSON.parse(await readFile(path, "utf8"));
+    return json;
+  } catch {
+    return {};
+  }
 }
