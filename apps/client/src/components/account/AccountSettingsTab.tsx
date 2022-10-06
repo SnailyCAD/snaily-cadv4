47a5cd9939d6cb8dbcdb262927acdcb487c7d338
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-10-06 14:39:27 +0200

    merge main

--- apps/client/src/components/account/AccountSettingsTab.tsx
+++ apps/client/src/components/account/AccountSettingsTab.tsx
@@ -9,7 +9,7 @@ import useFetch from "lib/useFetch";
 import { Button } from "components/Button";
 import { ChangePasswordArea } from "components/account/ChangePasswordArea";
 import { Manage2FAModal } from "./2fa/Manage2FAModal";
-import { Loader } from "components/Loader";
+import { Loader } from "@snailycad/ui";
 import { TwoFactorAuthArea } from "./2fa/TwoFactorAuthArea";
 import { handleValidate } from "lib/handleValidate";
 import type { PatchUserData } from "@snailycad/types/api";
