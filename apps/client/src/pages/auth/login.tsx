84af8541ed4ca185f423f377c3a61fc1e7ab0433
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-11-20 10:43:46 +0100

    :hammer: chore: add simple text

--- apps/client/src/pages/auth/login.tsx
+++ apps/client/src/pages/auth/login.tsx
@@ -31,6 +31,15 @@ export default function Login() {
         <LoginForm onFormSubmitted={handleSubmit} />
 
         <VersionDisplay cad={cad} />
+
+        <a
+          rel="noreferrer"
+          target="_blank"
+          className="mt-3 md:mt-0 relative md:absolute md:bottom-10 md:left-1/2 md:-translate-x-1/2 underline text-lg transition-colors text-neutral-700 hover:text-neutral-900 dark:text-gray-400 dark:hover:text-white mx-2 block cursor-pointer z-50"
+          href="https://snailycad.caspertheghost.me"
+        >
+          SnailyCAD
+        </a>
       </main>
     </>
   );
--- apps/client/src/pages/auth/register.tsx
+++ apps/client/src/pages/auth/register.tsx
@@ -182,6 +182,15 @@ function Register({ cad }: Props) {
           )}
         </Formik>
         <VersionDisplay cad={cad} />
+
+        <a
+          rel="noreferrer"
+          target="_blank"
+          className="mt-3 md:mt-0 relative md:absolute md:bottom-10 md:left-1/2 md:-translate-x-1/2 underline text-lg transition-colors text-neutral-700 hover:text-neutral-900 dark:text-gray-400 dark:hover:text-white mx-2 block cursor-pointer z-50"
+          href="https://snailycad.caspertheghost.me"
+        >
+          SnailyCAD
+        </a>
       </main>
     </>
   );
