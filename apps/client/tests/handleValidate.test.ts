34999e82f42cbda06cadfb7dadce3c18cc150ab0
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-11-22 19:00:30 +0100

    :hammer: chore: add tests for nested object validation

--- apps/client/tests/handleValidate.test.ts
+++ apps/client/tests/handleValidate.test.ts
@@ -1,6 +1,7 @@
 import { expect, it } from "vitest";
 import { handleValidate } from "../src/lib/handleValidate";
-import { AUTH_SCHEMA } from "@snailycad/schemas";
+import { AUTH_SCHEMA, LICENSE_SCHEMA } from "@snailycad/schemas";
+import subDays from "date-fns/subDays";
 
 it("Should correctly validate a CORRECT object, schema: AUTH_SCHEMA", () => {
   const data = {
@@ -24,4 +25,36 @@ it("Should correctly validate an INVALID object, schema: AUTH_SCHEMA", () => {
       "password": "String must contain at least 8 character(s)",
     }
   `);
+});
+
+it("Should correctly validate an INVALID nested object, schema: LICENSE_SCHEMA", () => {
+  const data = {
+    driversLicense: "testing",
+    pilotLicense: false,
+    suspended: {
+      driverLicense: true,
+      driverLicenseTimeEnd: subDays(new Date(), 3),
+      firearmsLicense: true,
+      pilotLicense: true,
+      waterLicense: false,
+    },
+  };
+
+  console.log({ data });
+
+  expect(handleValidate(LICENSE_SCHEMA)(data)).toMatchInlineSnapshot(
+    {
+      suspended: {
+        driverLicenseTimeEnd: expect.any(String),
+      },
+    },
+    `
+    {
+      "pilotLicense": "Expected string, received boolean",
+      "suspended": {
+        "driverLicenseTimeEnd": Any<String>,
+      },
+    }
+  `,
+  );
 });
