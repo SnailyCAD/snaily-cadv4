0ea92987f412d60d45c9f5f5a974b5285b3f358d
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-12-19 14:24:08 +0100

    :hammer: chore: minor fixes

--- apps/client/src/components/dispatch/map/units/UnitItem.tsx
+++ apps/client/src/components/dispatch/map/units/UnitItem.tsx
@@ -59,11 +59,11 @@ export function UnitItem({ setTempUnit, player }: CallItemProps) {
         </Accordion.Trigger>
         <Accordion.Content className="pt-2 text-base text-neutral-800 dark:text-white">
           <div className="map-column">
-            <Infofield label={t("status")}>{player.unit?.status?.value.value}</Infofield>
+            <Infofield label={t("status")}>{unit.status?.value?.value}</Infofield>
             <Infofield label={common("user")}>{player.username}</Infofield>
             {RADIO_CHANNEL_MANAGEMENT ? (
               <Infofield className="flex !flex-row gap-2 mt-1" label={t("radioChannel")}>
-                <UnitRadioChannelModal unit={player.unit!} />
+                <UnitRadioChannelModal unit={unit} />
               </Infofield>
             ) : null}
 
