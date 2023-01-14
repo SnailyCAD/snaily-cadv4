2a85923ca6ce61aab6ba25b6b743942d36c0744a
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-01-14 15:34:24 +0100

    chore: wrap in try/catch

--- packages/audit-logger/src/server.ts
+++ packages/audit-logger/src/server.ts
@@ -1,6 +1,7 @@
 import superjson from "superjson";
 import type { AuditLog, PrismaClient } from "@prisma/client";
 import type { AuditLogActions } from "./index";
+import { captureException } from "@sentry/node";
 
 export * from "./types/action-types";
 export * from "./types/actions";
@@ -19,18 +20,23 @@ interface Options<Action extends AuditLogActions> {
 export async function createAuditLogEntry<Action extends AuditLogActions>(
   options: Options<Action>,
 ) {
-  const auditLog = await options.prisma.auditLog.create({
-    data: {
-      translationKey: options.translationKey,
-      action: superjson.stringify(options.action),
-      executorId: options.executorId,
-    },
-  });
+  try {
+    const auditLog = await options.prisma.auditLog.create({
+      data: {
+        translationKey: options.translationKey,
+        action: superjson.stringify(options.action),
+        executorId: options.executorId,
+      },
+    });
 
-  return {
-    ...auditLog,
-    action: options.action,
-  };
+    return {
+      ...auditLog,
+      action: options.action,
+    };
+  } catch (error) {
+    captureException(error);
+    return null;
+  }
 }
 
 export function parseAuditLogs<T extends AuditLog | AuditLog[]>(log: T) {
