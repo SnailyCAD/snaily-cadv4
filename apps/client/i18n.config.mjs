bae727f12eba6090caa0a739894b77d8b6f3f6e4
Andor L <andorlavin@hotmail.com>
2023-01-25 09:50:37 +0100

    Nl be translation (#1467)
    <!--
    Thanks for opening a PR! Your contribution is much appreciated!
    -->
    
    
    ## Translations
    
    - [x] `.json` files are formatted: `yarn format`
    - [x] Translations are correct
    - [x] New translation? It's been added to `i18n.config.mjs`

--- apps/client/i18n.config.mjs
+++ apps/client/i18n.config.mjs
@@ -1,4 +1,4 @@
 export const i18n = {
-  locales: ["en", "en-gb", "ru", "cn", "tc", "fr-FR", "de-DE", "pt-BR", "cs-CZ"],
+  locales: ["en", "en-gb", "ru", "cn", "tc", "fr-FR", "de-DE", "pt-BR", "cs-CZ", "nl-BE"],
   defaultLocale: "en",
 };
