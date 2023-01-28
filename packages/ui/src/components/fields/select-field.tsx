6596bf8dcf9355dcc523af98ba3e477aa20b12f2
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-01-28 13:50:26 +0100

    chore: minor fixes

--- packages/ui/src/components/fields/select-field.tsx
+++ packages/ui/src/components/fields/select-field.tsx
@@ -79,6 +79,12 @@ export function SelectField<T extends SelectValue>(props: SelectFieldProps<T>) {
   const selectedItems = selectionMode === "multiple" ? state.selectedItems : null;
   const selectedItem = selectionMode === "single" ? state.selectedItems?.[0] : null;
 
+  console.log({
+    isOpen: state.isOpen,
+    isFocused: state.isFocused,
+    true: state.isOpen || state.isFocused,
+  });
+
   return (
     <ModalProvider>
       <div className={classNames("flex flex-col mb-3", props.className)}>
@@ -91,9 +97,7 @@ export function SelectField<T extends SelectValue>(props: SelectFieldProps<T>) {
               buttonVariants.default,
               buttonSizes.sm,
               "cursor-default rounded-md w-full h-10 flex items-center justify-between border !bg-white dark:!bg-secondary hover:dark:!bg-secondary hover:dark:!brightness-100",
-              (state.isOpen || state.isFocused) &&
-                !props.isDisabled &&
-                "dark:!border-gray-500 !border-gray-500",
+              (state.isOpen || state.isFocused) && "dark:!border-gray-500 !border-gray-500",
               props.isDisabled && "!cursor-not-allowed opacity-80",
               props.errorMessage
                 ? "!border-red-500 focus:!border-red-700 dark:focus:!border-red-700"
