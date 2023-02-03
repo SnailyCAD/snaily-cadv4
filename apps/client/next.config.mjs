4ecd863a929f5f68d771dd0760253f463a20c11a
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-02-03 10:12:37 +0100

    fix: TS errors

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -18,7 +18,7 @@ const nextConfig = {
   // prettier-ignore
   images: { // start images
     formats: ["image/avif", "image/webp"],
-    domains: ["i.imgur.com", "cdn.discordapp.com", "localhost"]
+    domains: ["i.imgur.com", "cdn.discordapp.com", "localhost", "172.20.10.2"]
   }, // end images
   // prettier-enable
   webpack(config, { webpack }) {
