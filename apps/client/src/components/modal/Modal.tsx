884d64fdadf7fb58c7ec4874173e403b2cffc36b
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-02-15 14:52:51 +0100

    Revert "fix: use radix-ui modal (closes #1528)"
    This reverts commit 44e221e0609143494bae083a6a59ac622fbba133.

--- apps/client/src/components/modal/Modal.tsx
+++ apps/client/src/components/modal/Modal.tsx
@@ -1,13 +1,5 @@
 import * as React from "react";
-import {
-  Root as DialogRoot,
-  DialogPortal,
-  DialogTitle,
-  DialogClose,
-  DialogContent,
-  DialogOverlay,
-} from "@radix-ui/react-dialog";
-import { Transition } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 import { X } from "react-bootstrap-icons";
 import { useModal } from "state/modalState";
 import { classNames } from "lib/classNames";
@@ -41,51 +33,73 @@ export function Modal({
   }, [canBeClosed, onClose]);
 
   return (
-    <Transition show={isOpen} appear>
-      <DialogRoot open={isOpen} onOpenChange={handleClose}>
-        <DialogPortal>
-          <DialogOverlay
-            className={classNames(
-              "overflow-y-auto fixed z-50 inset-0 bg-black/40 flex items-center justify-center w-full",
-              dialogClassName,
-            )}
+    <Transition show={isOpen} appear as={React.Fragment}>
+      <Dialog
+        open={isOpen}
+        as="div"
+        className={classNames(
+          "fixed inset-0 overflow-y-auto",
+          isAlert ? "z-[999]" : "z-50",
+          dialogClassName,
+        )}
+        onClose={handleClose}
+      >
+        <div className="min-h-screen px-4 text-center">
+          <Transition.Child
+            as={React.Fragment}
+            enter="ease-out duration-100"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            <Transition.Child
-              as={React.Fragment}
-              enter="ease-out duration-75"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-75"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
+            <div className="fixed inset-0 bg-black/40" />
+          </Transition.Child>
+
+          {/* this element is to trick the browser into centering the modal contents. */}
+          <span className="inline-block h-screen align-middle" aria-hidden="true">
+            &#8203;
+          </span>
+
+          <Transition.Child
+            as={React.Fragment}
+            enter="ease-out duration-75"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-75"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Dialog.Panel
+              style={modalStyles}
+              className={classNames(
+                "max-w-[100%] inline-block p-4 px-6 my-8 text-left align-middle transition-all transform bg-white border border-secondary dark:bg-tertiary dark:text-white shadow-xl rounded-lg",
+                isAlert ? "z-[998]" : "z-30",
+                className,
+              )}
             >
-              <DialogContent
-                style={modalStyles}
-                className={classNames(
-                  "inline-block max-md:!w-full p-4 px-6 my-8 text-left align-middle transition-all transform bg-white border border-secondary dark:bg-tertiary dark:text-white shadow-xl rounded-lg",
-                  isAlert ? "z-[998]" : "z-30",
-                  className,
-                )}
+              <Dialog.Title
+                as="h3"
+                className="flex items-center justify-between mb-2 text-2xl font-semibold text-gray-900 dark:text-white"
               >
-                <DialogTitle className="flex items-center justify-between mb-2 text-2xl font-semibold text-gray-900 dark:text-white">
-                  {title}
+                {title}
 
-                  <DialogClose
-                    type="button"
-                    aria-label="Close Modal"
-                    onClick={onClose}
-                    className="p-1.5 transition-all cursor-pointer rounded-lg hover:bg-gray-200 dark:hover:bg-secondary"
-                  >
-                    <X width={25} height={25} />
-                  </DialogClose>
-                </DialogTitle>
+                <button
+                  type="button"
+                  aria-label="Close Modal"
+                  onClick={onClose}
+                  className="p-1.5 transition-all cursor-pointer rounded-lg hover:bg-gray-200 dark:hover:bg-secondary"
+                >
+                  <X width={25} height={25} />
+                </button>
+              </Dialog.Title>
 
-                {children}
-              </DialogContent>
-            </Transition.Child>
-          </DialogOverlay>
-        </DialogPortal>
-      </DialogRoot>
+              {children}
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
     </Transition>
   );
 }
