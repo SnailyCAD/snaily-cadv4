fd3def1a42f797c207b43756c1aef412a3235386
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-02-25 08:28:59 +0100

    chore: minor fixes

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -18,7 +18,8 @@ export const buttonVariants = {
   transparent: "bg-transparent text-black dark:text-white",
   link: "bg-transparent hover:bg-transparent text-gray-800 dark:text-gray-200 px-0 p-1 underline",
   blue: "bg-blue-500 dark:bg-blue-500 hover:bg-blue-600 dark:hover:bg-blue-600 dark:border dark:border-blue-800 text-black",
-  amber: "text-black bg-orange-500 dark:bg-orange-500 hover:bg-orange-600 dark:hover:bg-orange-600",
+  amber:
+    "text-black bg-orange-500 dark:bg-orange-500 border border-amber-800 hover:bg-orange-600 dark:hover:bg-orange-600",
 };
 
 export const buttonSizes = {
