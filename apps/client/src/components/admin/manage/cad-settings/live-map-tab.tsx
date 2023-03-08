006882d97ef19c1e1b8c6ad1398453c35231dd79
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-03-08 14:40:26 +0100

    :tada: feat: custom tab for live map

---
+++ apps/client/src/components/admin/manage/cad-settings/live-map-tab.tsx
@@ -0,0 +1,95 @@
+import { Button, Input, Loader, TabsContent } from "@snailycad/ui";
+import { useAuth } from "context/AuthContext";
+import { Form, Formik, FormikHelpers } from "formik";
+import useFetch from "lib/useFetch";
+import { useTranslations } from "use-intl";
+import { SettingsFormField } from "components/form/SettingsFormField";
+import { SettingsTabs } from "src/pages/admin/manage/cad-settings";
+import { toastMessage } from "lib/toastMessage";
+import type { PutCADApiTokenData } from "@snailycad/types/api";
+import Link from "next/link";
+import { BoxArrowUpRight } from "react-bootstrap-icons";
+
+export function LiveMapTab() {
+  const common = useTranslations("Common");
+  const { state, execute } = useFetch();
+  const { cad, setCad } = useAuth();
+  const t = useTranslations("LiveMapTab");
+
+  async function onSubmit(
+    values: typeof INITIAL_VALUES,
+    helpers: FormikHelpers<typeof INITIAL_VALUES>,
+  ) {
+    if (!cad) return;
+
+    const { json } = await execute<PutCADApiTokenData>({
+      path: "/admin/manage/cad-settings/api-token",
+      method: "PUT",
+      data: values,
+    });
+
+    setCad({ ...cad, apiTokenId: json?.id ?? null, apiToken: json });
+    toastMessage({
+      icon: "success",
+      title: common("success"),
+      message: common("savedSettingsSuccess"),
+    });
+
+    if (json) {
+      helpers.setFieldValue("token", json.token);
+    }
+  }
+
+  const INITIAL_VALUES = {
+    liveMapURL: cad?.miscCadSettings?.liveMapURL ?? "",
+  };
+
+  return (
+    <TabsContent aria-label={t("liveMap")} value={SettingsTabs.LiveMap}>
+      <h2 className="mt-2 text-2xl font-semibold">{t("liveMapSettings")}</h2>
+
+      <p className="my-3 text-neutral-700 dark:text-gray-400 max-w-2xl">
+        {t("liveMapSettingsInfo")}
+      </p>
+
+      <Formik onSubmit={onSubmit} initialValues={INITIAL_VALUES}>
+        {({ handleChange, errors, values }) => (
+          <Form className="mt-3 space-y-5">
+            <SettingsFormField
+              description={
+                <span>
+                  This URL will communicate to the live_map resource in your FiveM server.{" "}
+                  <Link
+                    className="mt-1 underline inline-flex items-center gap-1 text-neutral-700 dark:text-gray-200"
+                    target="_blank"
+                    href="https://docs.snailycad.org/docs/fivem-integrations/live-map"
+                  >
+                    Learn more
+                    <BoxArrowUpRight className="inline-block" />
+                  </Link>
+                </span>
+              }
+              errorMessage={errors.liveMapURL}
+              label="Live Map URL"
+            >
+              <Input
+                type="url"
+                name="liveMapURL"
+                value={values.liveMapURL}
+                onChange={handleChange}
+                placeholder="http://my-host:my-port"
+              />
+            </SettingsFormField>
+
+            <div className="flex">
+              <Button className="flex items-center" type="submit" disabled={state === "loading"}>
+                {state === "loading" ? <Loader className="mr-3 border-red-300" /> : null}
+                {common("save")}
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </TabsContent>
+  );
+}
