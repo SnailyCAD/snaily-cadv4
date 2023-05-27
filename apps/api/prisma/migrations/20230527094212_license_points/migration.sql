bb12168abfa46063008eac806e7dc42935871df3
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-05-27 11:42:39 +0200

    chore: 1 migration

--- apps/api/prisma/migrations/20230527094212_license_points/migration.sql
+++ apps/api/prisma/migrations/20230527094212_license_points/migration.sql
@@ -11,13 +11,9 @@ ADD COLUMN     "weaponLicenseMaxPoints" INTEGER DEFAULT 12;
 CREATE TABLE "CitizenLicensePoints" (
     "id" TEXT NOT NULL,
     "driverLicensePoints" INTEGER NOT NULL DEFAULT 0,
-    "driverLastResetAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "pilotLicensePoints" INTEGER NOT NULL DEFAULT 0,
-    "pilotLastResetAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "waterLicensePoints" INTEGER NOT NULL DEFAULT 0,
-    "waterLastResetAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "firearmsLicensePoints" INTEGER NOT NULL DEFAULT 0,
-    "firearmsLastResetAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "CitizenLicensePoints_pkey" PRIMARY KEY ("id")
