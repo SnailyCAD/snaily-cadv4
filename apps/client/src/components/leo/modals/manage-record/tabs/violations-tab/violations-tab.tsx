dd30766eee6a2d9e8ef5150132f24b19b0a27c87
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-06-07 14:27:01 +0200

    feat: add migration

--- apps/client/src/components/leo/modals/manage-record/tabs/violations-tab/violations-tab.tsx
+++ apps/client/src/components/leo/modals/manage-record/tabs/violations-tab/violations-tab.tsx
@@ -1,10 +1,8 @@
 import type { PenalCode } from "@snailycad/types";
-import { Button, TabsContent } from "@snailycad/ui";
+import { TabsContent } from "@snailycad/ui";
 import { FormField } from "components/form/FormField";
 import type { SelectValue } from "components/form/Select";
 import { useFormikContext } from "formik";
-import { useModal } from "state/modalState";
-import { ModalIds } from "types/ModalIds";
 import { useTranslations } from "use-intl";
 import { PenalCodesTable } from "../../penal-codes-table";
 import { SelectPenalCode } from "../../select-penal-code";
@@ -20,21 +18,12 @@ interface _FormikContext {
 
 export function ViolationsTab(props: ViolationsTabProps) {
   const t = useTranslations("Leo");
-  const { openModal } = useModal();
   const { handleChange, values } = useFormikContext<_FormikContext>();
 
   return (
     <TabsContent value="violations-tab">
       <header className="flex items-center justify-between">
         <h3 className="text-xl font-semibold">{t("violations")}</h3>
-
-        <Button
-          type="button"
-          onPress={() => openModal(ModalIds.ManageSeizedItems)}
-          disabled={props.isReadOnly}
-        >
-          {t("add")}
-        </Button>
       </header>
 
       <FormField label={t("violations")}>
