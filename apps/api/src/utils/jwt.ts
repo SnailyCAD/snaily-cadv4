afb4515890bb26da3c53ec9647ac9131fe03ddda
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-06-11 19:52:12 +0200

    fix: require JWT_SECRET to be set

--- apps/api/src/utils/jwt.ts
+++ apps/api/src/utils/jwt.ts
@@ -1,22 +1,21 @@
 import process from "node:process";
-import { $log } from "@tsed/logger";
 import { sign, verify } from "jsonwebtoken";
 
 export function signJWT(value: any, expiresInSeconds: number | string) {
-  const secret = process.env.JWT_SECRET ?? "NONE";
+  const secret = process.env.JWT_SECRET;
 
-  if (secret === "NONE") {
-    $log.warn("No JWT_SECRET env var was found");
+  if (!secret) {
+    throw new Error("No JWT_SECRET env var was found");
   }
 
   return sign(value, secret, { expiresIn: expiresInSeconds });
 }
 
 export function verifyJWT(value: string) {
-  const secret = process.env.JWT_SECRET ?? "NONE";
+  const secret = process.env.JWT_SECRET;
 
-  if (secret === "NONE") {
-    $log.warn("No JWT_SECRET env var was found");
+  if (!secret) {
+    throw new Error("No JWT_SECRET env var was found");
   }
 
   try {
