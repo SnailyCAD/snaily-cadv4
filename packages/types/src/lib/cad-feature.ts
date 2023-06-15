0e4ec6fffb7eb279a6d397e247e9016019b29d04
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-06-15 17:21:07 +0200

    feat: ability to select courthouse types

--- packages/types/src/lib/cad-feature.ts
+++ packages/types/src/lib/cad-feature.ts
@@ -2,6 +2,8 @@ import * as Enums from "../enums";
 
 export interface CadFeatureOptions {
   [Enums.Feature.LICENSE_EXAMS]: LicenseExamFeatureOption;
+  [Enums.Feature.COURTHOUSE]: CourthouseFeatureOption;
 }
 
 export type LicenseExamFeatureOption = Enums.LicenseExamType[];
+export type CourthouseFeatureOption = Enums.CourthouseType[];
