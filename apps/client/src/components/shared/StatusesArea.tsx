d295892187621747a4e6a7d0cf3a126a919358db
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-08-04 12:34:35 +0200

    fix: add warning of no 10-code on duty is present

--- apps/client/src/components/shared/StatusesArea.tsx
+++ apps/client/src/components/shared/StatusesArea.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 import { useListener } from "@casper124578/use-socket.io";
 import { SocketEvents } from "@snailycad/config";
 import { Button } from "@snailycad/ui";
@@ -140,17 +141,27 @@ export function StatusesArea<T extends ActiveOfficer | ActiveDeputy>({
   const filteredCodes = codes10.values.filter((v) => handleWhatPagesFilter(v, whatPagesType));
   const onDutyCode = filteredCodes.find((v) => v.shouldDo === ShouldDoType.SET_ON_DUTY);
   const isOnDutyActive = !isUnitOffDuty && onDutyCode?.id === activeUnit.status?.id;
+  const documentationUrl = createValueDocumentationURL(ValueType.CODES_10);
 
-  if (!onDutyCode && filteredCodes.length <= 0) {
-    const documentationUrl = createValueDocumentationURL(ValueType.CODES_10);
+  if (!onDutyCode) {
+    <Link
+      target="_blank"
+      href={documentationUrl}
+      className="block mt-2 px-4 py-3 bg-gray-300/50 dark:bg-tertiary dark:border-t-[1.5px] dark:border-secondary text-blue-500 dark:text-blue-400 underline"
+    >
+      This SnailyCAD instance does not have a 10-code for setting a unit on duty. Please ask an
+      admin to add one with the {`"Should Do"`} set to {`"Set On Duty"`}.
+    </Link>;
+  }
 
+  if (filteredCodes.length <= 0) {
     return (
       <Link
         target="_blank"
         href={documentationUrl}
         className="block mt-2 px-4 py-3 bg-gray-300/50 dark:bg-tertiary dark:border-t-[1.5px] dark:border-secondary text-blue-500 dark:text-blue-400 underline"
       >
-        This CAD does not have any 10 codes. Please ask an admin to add some.
+        This SnailyCAD instance does not have any 10 codes. Please ask an admin to add some.
       </Link>
     );
   }
