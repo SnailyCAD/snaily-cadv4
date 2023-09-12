8bd533e9dd4f177e1fe8f81dd5a1bc5e7dea2c77
casperiv0 <53900565+casperiv0@users.noreply.github.com>
2023-09-12 16:21:19 +0200

    fix: license exams

---
+++ apps/api/prisma/migrations/20230912142000_license_exams_fix/migration.sql
@@ -0,0 +1,69 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `licenseExamId` on the `DriversLicenseCategoryValue` table. All the data in the column will be lost.
+  - You are about to drop the `DLExam` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `WeaponExam` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `_DLExamToDriversLicenseCategoryValue` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `_DriversLicenseCategoryValueToWeaponExam` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "DLExam" DROP CONSTRAINT "DLExam_citizenId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "DLExam" DROP CONSTRAINT "DLExam_licenseId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "DriversLicenseCategoryValue" DROP CONSTRAINT "DriversLicenseCategoryValue_licenseExamId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "WeaponExam" DROP CONSTRAINT "WeaponExam_citizenId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "WeaponExam" DROP CONSTRAINT "WeaponExam_licenseId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_DLExamToDriversLicenseCategoryValue" DROP CONSTRAINT "_DLExamToDriversLicenseCategoryValue_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_DLExamToDriversLicenseCategoryValue" DROP CONSTRAINT "_DLExamToDriversLicenseCategoryValue_B_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_DriversLicenseCategoryValueToWeaponExam" DROP CONSTRAINT "_DriversLicenseCategoryValueToWeaponExam_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_DriversLicenseCategoryValueToWeaponExam" DROP CONSTRAINT "_DriversLicenseCategoryValueToWeaponExam_B_fkey";
+
+-- AlterTable
+ALTER TABLE "DriversLicenseCategoryValue" DROP COLUMN "licenseExamId";
+
+-- DropTable
+DROP TABLE "DLExam";
+
+-- DropTable
+DROP TABLE "WeaponExam";
+
+-- DropTable
+DROP TABLE "_DLExamToDriversLicenseCategoryValue";
+
+-- DropTable
+DROP TABLE "_DriversLicenseCategoryValueToWeaponExam";
+
+-- CreateTable
+CREATE TABLE "_dlCategoryToLicenseExam" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_dlCategoryToLicenseExam_AB_unique" ON "_dlCategoryToLicenseExam"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_dlCategoryToLicenseExam_B_index" ON "_dlCategoryToLicenseExam"("B");
+
+-- AddForeignKey
+ALTER TABLE "_dlCategoryToLicenseExam" ADD CONSTRAINT "_dlCategoryToLicenseExam_A_fkey" FOREIGN KEY ("A") REFERENCES "DriversLicenseCategoryValue"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_dlCategoryToLicenseExam" ADD CONSTRAINT "_dlCategoryToLicenseExam_B_fkey" FOREIGN KEY ("B") REFERENCES "LicenseExam"("id") ON DELETE CASCADE ON UPDATE CASCADE;
