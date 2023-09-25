cf467623a576055155a2b7e341937206b6ca1b6e
casperiv0 <53900565+casperiv0@users.noreply.github.com>
2023-09-25 16:29:57 +0200

    feat: open law book

--- apps/api/src/controllers/admin/admin-controller.ts
+++ apps/api/src/controllers/admin/admin-controller.ts
@@ -126,18 +126,12 @@ export class AdminController {
         { headers: { "user-agent": "SnailyCAD", accept: "application/vnd.github+json" } },
       );
 
-      console.log({ response });
-
       res.setHeader(
         "Cache-Control",
         `private, max-age=${ONE_DAY} stale-while-revalidate=${ONE_DAY / 2}`,
       );
 
       const body = (await response.body.json()) as { body: string };
-
-      console.log({
-        body,
-      });
 
       this.changelogBody = body.body;
       const json = body.body;
