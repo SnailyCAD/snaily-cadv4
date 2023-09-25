19fe35ba7f2a20b2cea6b7de95846081b5795bb9
casperiv0 <53900565+casperiv0@users.noreply.github.com>
2023-09-25 17:03:37 +0200

    feat: extended EMS/FD incidents

--- apps/client/src/components/leo/incidents/manage-incident-modal.tsx
+++ apps/client/src/components/leo/incidents/manage-incident-modal.tsx
@@ -7,6 +7,7 @@ import {
   FormRow,
   Infofield,
   FullDate,
+  TextField,
 } from "@snailycad/ui";
 import { FormField } from "components/form/FormField";
 import { Modal } from "components/modal/Modal";
@@ -160,16 +161,20 @@ export function ManageIncidentModal<T extends LeoIncident | EmsFdIncident>({
   }
 
   const validate = handleValidate(LEO_INCIDENT_SCHEMA);
+  const isEmsFdIncident = incident && "fireType" in incident;
+
   const INITIAL_VALUES = {
     description: incident?.description ?? "",
     postal: incident?.postal ?? "",
     descriptionData: dataToSlate(incident),
+    vehicleInvolved: isEmsFdIncident ? incident.vehicleInvolved : false,
     firearmsInvolved: incident?.firearmsInvolved ?? false,
     injuriesOrFatalities: incident?.injuriesOrFatalities ?? false,
     arrestsMade: incident?.arrestsMade ?? false,
     isActive: isDispatch ? true : incident?.isActive ?? false,
     situationCodeId: incident?.situationCodeId ?? null,
     openModalAfterCreation: true,
+    fireType: isEmsFdIncident ? incident.fireType ?? "" : "",
   };
 
   return (
@@ -205,7 +210,12 @@ export function ManageIncidentModal<T extends LeoIncident | EmsFdIncident>({
               ) : null}
 
               <div>
-                <FormRow className="mb-3" useFlex>
+                <div
+                  className={classNames(
+                    "grid mb-3",
+                    type === "ems-fd" ? "grid-cols-2" : "grid-cols-3",
+                  )}
+                >
                   <SwitchField
                     isDisabled={areFieldsDisabled}
                     isSelected={values.firearmsInvolved}
@@ -232,7 +242,16 @@ export function ManageIncidentModal<T extends LeoIncident | EmsFdIncident>({
                   >
                     {t("arrestsMade")}
                   </SwitchField>
-                </FormRow>
+
+                  <SwitchField
+                    isDisabled={areFieldsDisabled}
+                    isSelected={values.vehicleInvolved}
+                    onChange={(isSelected) => setFieldValue("vehicleInvolved", isSelected)}
+                    className="w-full"
+                  >
+                    {t("vehicleInvolved")}
+                  </SwitchField>
+                </div>
 
                 <FormField errorMessage={errors.description} label={common("description")}>
                   <Editor
@@ -242,22 +261,33 @@ export function ManageIncidentModal<T extends LeoIncident | EmsFdIncident>({
                   />
                 </FormField>
 
-                <FormRow useFlex>
-                  <ValueSelectField
-                    className="w-full"
-                    isOptional
-                    isDisabled={areFieldsDisabled}
-                    isClearable
-                    label={t("situationCode")}
-                    fieldName="situationCodeId"
-                    values={codes10.values}
-                    valueType={ValueType.CODES_10}
-                    filterFn={(value) => value.type === StatusValueType.SITUATION_CODE}
-                  />
+                <FormRow useFlex className={classNames(type === "ems-fd" && "!flex-col")}>
+                  <FormRow useFlex className={classNames(type === "leo" && "!flex-col")}>
+                    <ValueSelectField
+                      className="w-full"
+                      isOptional
+                      isDisabled={areFieldsDisabled}
+                      isClearable
+                      label={t("situationCode")}
+                      fieldName="situationCodeId"
+                      values={codes10.values}
+                      valueType={ValueType.CODES_10}
+                      filterFn={(value) => value.type === StatusValueType.SITUATION_CODE}
+                    />
+
+                    <TextField
+                      isOptional
+                      value={values.fireType}
+                      className="w-full"
+                      onChange={(value) => setFieldValue("fireType", value)}
+                      label={t("fireType")}
+                    />
+                  </FormRow>
 
                   <AddressPostalSelect
+                    addressOptional
                     isDisabled={areFieldsDisabled}
-                    postalOnly
+                    postalOnly={type === "leo"}
                     addressLabel="location"
                   />
                 </FormRow>
