b1bac10998586a221601be3c1fd5ea0acf2b53ce
casperiv0 <53900565+casperiv0@users.noreply.github.com>
2023-10-06 18:51:52 +0200

    fix: show correct status text color

--- apps/client/src/components/dispatch/active-units/deputies/active-deputies.tsx
+++ apps/client/src/components/dispatch/active-units/deputies/active-deputies.tsx
@@ -128,6 +128,7 @@ function ActiveDeputies({ initialDeputies }: Props) {
               name: nameAndCallsign,
               deputy: (
                 <DeputyColumn
+                  textColor={textColor}
                   deputy={deputy}
                   isDispatch={isDispatch}
                   nameAndCallsign={nameAndCallsign}
