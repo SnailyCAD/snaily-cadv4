1f0ab5d627be9a35ff5b3cda7fa012c4622fab52
casperiv0 <53900565+casperiv0@users.noreply.github.com>
2023-10-26 08:32:51 +0200

    fix: incorrect sorting orders

--- apps/client/src/pages/admin/values/[path].tsx
+++ apps/client/src/pages/admin/values/[path].tsx
@@ -84,14 +84,37 @@ export default function ValuePath({ pathValues: { totalCount, type, values: data
     valueTypes: pathsRecord[type] ? pathsRecord[type] : [],
   });
 
+  const isBaseObj = (
+    [
+      ValueType.ADDRESS_FLAG,
+      ValueType.BLOOD_GROUP,
+      ValueType.CITIZEN_FLAG,
+      ValueType.VEHICLE_FLAG,
+      ValueType.ETHNICITY,
+      ValueType.GENDER,
+      ValueType.IMPOUND_LOT,
+      ValueType.OFFICER_RANK,
+      ValueType.VEHICLE_TRIM_LEVEL,
+      ValueType.WEAPON_FLAG,
+    ] as string[]
+  ).includes(type);
   const [search, setSearch] = React.useState("");
   const asyncTable = useAsyncTable({
     search,
     sortingSchema: {
-      value: "value.value",
+      value: isBaseObj ? "value" : "value.value",
       gameHash: "hash",
-      isDisabled: "value.isDisabled",
-      createdAt: "value.createdAt",
+      isDisabled: isBaseObj ? "isDisabled" : "value.isDisabled",
+      createdAt: isBaseObj ? "createdAt" : "value.createdAt",
+      pairedUnitTemplate: "pairedUnitTemplate",
+      department: "department.value.value",
+      callsign: "callsign",
+      type: "type",
+      whitelisted: "whitelisted",
+      isDefaultDepartment: "isDefaultDepartment",
+      defaultOfficerRank: "defaultOfficerRank.value",
+      color: "color",
+      shouldDo: "shouldDo",
     },
     fetchOptions: {
       onResponse(json: GetValuesData) {
