681b20eb66bed30e6032385a6ea2c54760798918
casperiv0 <53900565+casperiv0@users.noreply.github.com>
2023-11-26 17:18:17 +0100

    fix: windows build

--- apps/api/scripts/postbuild.mjs
+++ apps/api/scripts/postbuild.mjs
@@ -1,8 +1,8 @@
 import { cp } from "node:fs/promises";
 import { resolve } from "node:path";
 
-const currentDir = new URL(import.meta.url).pathname;
-const templatesDir = resolve(currentDir, "../../src/templates");
+const currentDir = process.cwd();
+const templatesDir = resolve(currentDir, "src/templates");
 
 // The templates directory isn't copied over by SWC. Therefore, we have to do this manually.
-cp(templatesDir, resolve(currentDir, "../../dist/templates"), { recursive: true });
+cp(templatesDir, resolve(currentDir, "dist/templates"), { recursive: true });
