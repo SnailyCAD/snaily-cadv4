158f3e3cf3adbcef7827e33ab9a8bcb6128ad47a
casperiv0 <53900565+casperiv0@users.noreply.github.com>
2024-06-09 16:46:54 +0200

    fix: upgrade Sentry

--- apps/api/src/instrument.ts
+++ apps/api/src/instrument.ts
@@ -1,11 +1,14 @@
 import * as Sentry from "@sentry/node";
-import { prisma } from "lib/data/prisma";
 
 Sentry.init({
   dsn: "https://308dd96b826c4e38a814fc9bae681687@o518232.ingest.sentry.io/6553288",
   integrations: [
-    new Sentry.Integrations.Http({ tracing: true }),
-    new Sentry.Integrations.Prisma({ client: prisma }),
+    Sentry.httpIntegration(),
+    Sentry.prismaIntegration(),
+    Sentry.nestIntegration(),
+    Sentry.consoleIntegration(),
+    Sentry.onUnhandledRejectionIntegration(),
+    Sentry.onUncaughtExceptionIntegration(),
   ],
   tracesSampleRate: 1.0,
   profilesSampleRate: 1.0,
